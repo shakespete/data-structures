a173abf2f9aa013a4f8759564ea4d939dc919db9
pete <petersyjuco@gmail.com>
2020-03-19 21:49:04 +0800

    Daily Kata: Trie

--- Daily Kata/main.cpp
+++ Daily Kata/main.cpp
@@ -1,77 +1,102 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-struct Node {
-	const char* val;
-	Node* prev;
-	Node* next;
-};
-class DLL {
+// Trie Node
+class TrieNode {
 public:
-	DLL();
-	~DLL();
-	bool empty() const;
-	Node* front() const;
-	Node* back() const;
-	void add(Node* v, const char* e);
-	void addFront(const char* e);
-	void addBack(const char* e);
-	void remove(Node* v);
-	void removeFront();
-	void removeBack();
-private:
-	Node* head;
-	Node* tail;
+	TrieNode();
+	int key;
+	bool isEnd;
+	bool isDel;
+	TrieNode** children;
 };
-DLL::DLL() {
-	head = new Node;
-	tail = new Node;
-	head->next = tail;
-	tail->prev = head;
-	head->val = NULL;
-	tail->val = NULL;
-}
-DLL::~DLL() { while (!empty()) removeFront(); }
-bool DLL::empty() const { return head->next == tail; }
-Node* DLL::front() const { return empty() ? NULL : head->next; }
-Node* DLL::back() const { return empty() ? NULL : tail->prev; }
-void DLL::add(Node* v, const char* e) {
-	Node* u = new Node;
-	u->val = e;
+TrieNode::TrieNode() {
+	key = 0;
+	isEnd = false;
+	isDel = false;
 
-	u->next = v;
-	u->prev = v->prev;
-	v->prev->next = u;
-	v->prev = u;
-}
-void DLL::addFront(const char* e) { add(head->next, e); }
-void DLL::addBack(const char* e) { add(tail, e); }
-void DLL::remove(Node* v) {
-	if (!empty()) {
-		Node* u = v->prev;
-		Node* w = v->next;
-		u->next = w;
-		w->prev = u;
-		delete v;
+	children = new TrieNode * [26];
+	for (int i = 0; i < 26; ++i) {
+		children[i] = NULL;
 	}
 }
-void DLL::removeFront() { if (!empty()) remove(head->next); }
-void DLL::removeBack() { if (!empty()) remove(tail->prev); }
 
+// Trie
+class Trie {
+public:
+	Trie();
+	void insert(const char* str, int k);
+	int find(const char* str);
+	void remove(char* str);
+	TrieNode* root;
+};
 
+Trie::Trie() { root = new TrieNode(); }
+void Trie::insert(const char* str, int k) {
+	TrieNode* crawler = root;
+
+	int ctr = 0;
+	int index;
+
+	while (str[ctr] != '\0') {
+		index = str[ctr] - 'a';
+
+		if (crawler->children[index] == NULL)
+			crawler->children[index] = new TrieNode();
+
+		crawler = crawler->children[index];
+		ctr++;
+	}
+
+	crawler->isEnd = true;
+	crawler->key = k;
+}
+int Trie::find(const char* str) {
+	TrieNode* crawler = root;
+	int ctr = 0;
+	int index;
+
+	while (str[ctr] != '\0') {
+		index = str[ctr] - 'a';
+
+		if (crawler->children[index] == NULL) return -1;
+
+		crawler = crawler->children[index];
+		ctr++;
+	}
+
+	if (crawler->isEnd && !crawler->isDel) return crawler->key;
+	return -1;
+}
+void Trie::remove(char* str) {
+	TrieNode* crawler = root;
+	int ctr = 0;
+	int index;
+
+	while (str[ctr] != '\0') {
+		index = str[ctr] - 'a';
+
+		if (crawler->children[index] == NULL) return;
+
+		crawler = crawler->children[index];
+		ctr++;
+	}
+
+	if (crawler->isEnd) crawler->isDel = true;
+}
+
+int uniqId = 0;
 int main() {
-	DLL* dList = new DLL();
-	dList->addBack("hello");
-	dList->addBack("darkness");
-	dList->addBack("my");
-	dList->addBack("old");
-	dList->addBack("friend");
+	Trie* t = new Trie();
+	t->insert("sing", ++uniqId);
+	t->insert("sip", ++uniqId);
+	t->insert("ask", ++uniqId);
 
-	while (!dList->empty()) {
-		printf("%s ", dList->front()->val);
-		dList->removeFront();
-	}
-	printf("\n");
-
+	printf("%d\n", t->find("sing"));
+	printf("%d\n", t->find("sip"));
+	printf("%d\n", t->find("ask"));
+	printf("%d\n", t->find("sin"));
+	printf("%d\n", t->find("as"));
+	
 	return 0;
 }
