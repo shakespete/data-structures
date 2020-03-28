3fd5ac05eeeb1f97c84651b5e4b14fd708a6e627
pete <petersyjuco@gmail.com>
2020-03-28 21:37:11 +0800

    Daily Kata: Queue

--- Daily Kata/main.cpp
+++ Daily Kata/main.cpp
@@ -3,54 +3,91 @@
 
 struct Node {
 	const char* val;
+	Node* next;
 };
 
-class Stack {
-	enum { DEF_CAP = 47 };
+class CLL {
 public:
-	Stack(int cap = DEF_CAP);
-	~Stack();
-	int size() const;
+	CLL();
+	~CLL();
 	bool empty() const;
-	Node* top() const;
-	void push(const char* e);
-	void pop();
+	Node* front() const;
+	Node* back() const;
+	void add(const char* e);
+	void advance();
+	void remove();
 private:
-	Node** S;
-	int t;
-	int capacity;
+	Node* cursor;
 };
-Stack::Stack(int cap) : S(new Node*[cap]), t(-1), capacity(cap) { }
-Stack::~Stack() { while (!empty()) pop(); }
-int Stack::size() const { return t + 1; }
-bool Stack::empty() const { return size() == 0; }
-Node* Stack::top() const { return empty() ? NULL : S[t]; }
-void Stack::push(const char* e) {
-	if (size() != capacity) {
-		Node* v = new Node;
-		v->val = e;
-		++t;
-		S[t] = v;
+
+CLL::CLL() : cursor(NULL) { }
+CLL::~CLL() { while (!empty()) remove(); }
+bool CLL::empty() const { return cursor == NULL; }
+Node* CLL::front() const { return empty() ? NULL : cursor->next; }
+Node* CLL::back() const { return empty() ? NULL : cursor; }
+void CLL::add(const char* e) {
+	Node* v = new Node;
+	v->val = e;
+	if (empty()) {
+		v->next = v;
+		cursor = v;
+	}
+	else {
+		v->next = cursor->next;
+		cursor->next = v;
 	}
 }
-void Stack::pop() {
+void CLL::advance() { if (!empty()) cursor = cursor->next; }
+void CLL::remove() {
 	if (!empty()) {
-		delete S[t];
-		--t;
+		Node* old = cursor->next;
+		if (old == cursor) cursor = NULL;
+		else cursor->next = old->next;
+		delete old;
+	}
+}
+
+class Queue {
+public:
+	Queue();
+	~Queue();
+	int size() const;
+	bool empty() const;
+	Node* front() const;
+	void enq(const char* e);
+	void deq();
+private:
+	CLL CL;
+	int n;
+};
+Queue::Queue() : CL(), n(0) { }
+Queue::~Queue() { while (!empty()) deq(); }
+int Queue::size() const { return n; }
+bool Queue::empty() const { return n == 0; }
+Node* Queue::front() const { return empty() ? NULL : CL.front(); }
+void Queue::enq(const char* e) {
+	CL.add(e);
+	CL.advance();
+	++n;
+}
+void Queue::deq() {
+	if (!empty()) {
+		CL.remove();
+		--n;
 	}
 }
 
 int main() {
-	Stack* st = new Stack();
-	st->push("friend");
-	st->push("old");
-	st->push("my");
-	st->push("darkness");
-	st->push("hello");
+	Queue* q = new Queue();
+	q->enq("Hello");
+	q->enq("darkness");
+	q->enq("my");
+	q->enq("old");
+	q->enq("friend");
 
-	while (!st->empty()) {
-		printf("%s ", st->top()->val);
-		st->pop();
+	while (!q->empty()) {
+		printf("%s ", q->front()->val);
+		q->deq();
 	}
 	printf("\n");
 	return 0;
