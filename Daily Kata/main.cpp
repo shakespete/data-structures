87024898a5faff90e5d450b79b96e4536c9f883e
pete <petersyjuco@gmail.com>
2020-05-12 22:24:16 +0800

    Daily Kata: Linked List

--- Daily Kata/main.cpp
+++ Daily Kata/main.cpp
@@ -1,103 +1,52 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-class TrieNode {
-public:
-	TrieNode();
-	int key;
-	bool isEnd;
-	bool isDel;
-	TrieNode** children;
+struct Node {
+	const char* val;
+	Node* next;
 };
 
-TrieNode::TrieNode() {
-	key = -1;
-	isEnd = false;
-	isDel = false;
-
-	children = new TrieNode * [26];
-	for (int i = 0; i < 26; ++i)
-		children[i] = NULL;
-}
-
-class Trie {
+class LL {
 public:
-	Trie();
-	void insert(const char* str, const int k);
-	int find(const char* str);
-	void remove(const char* str);
+	LL();
+	~LL();
+	bool empty() const;
+	Node* front() const;
+	void add(const char* e);
+	void remove();
 private:
-	TrieNode* root;
+	Node* head;
 };
 
-
-Trie::Trie() { root = new TrieNode(); }
-void Trie::insert(const char* str, const int k) {
-	TrieNode* crawler = root;
-
-	int ctr = 0;
-	int idx;
-	while (str[ctr] != '\0') {
-		idx = str[ctr] - 'a';
-
-		if (crawler->children[idx] == NULL)
-			crawler->children[idx] = new TrieNode();
-
-		crawler = crawler->children[idx];
-		++ctr;
-	}
-
-	crawler->key = k;
-	crawler->isEnd = true;
+LL::LL() : head(NULL) { }
+LL::~LL() { while (!empty()) remove(); }
+bool LL::empty() const { return head == NULL; }
+Node* LL::front() const { return empty() ? NULL : head; }
+void LL::add(const char* e) {
+	Node* v = new Node;
+	v->val = e;
+	v->next = head;
+	head = v;
 }
-int Trie::find(const char* str) {
-	TrieNode* crawler = root;
-
-	int ctr = 0;
-	int idx;
-	while (str[ctr] != '\0') {
-		idx = str[ctr] - 'a';
-
-		if (crawler->children[idx] == NULL) return -1;
-
-		crawler = crawler->children[idx];
-		++ctr;
+void LL::remove() {
+	if (!empty()) {
+		Node* old = head;
+		head = old->next;
+		delete old;
 	}
-
-	if (crawler->isEnd && !crawler->isDel) return crawler->key;
-	return -1;
-}
-void Trie::remove(const char* str) {
-	TrieNode* crawler = root;
-
-	int ctr = 0;
-	int idx;
-	while (str[ctr] != '\0') {
-		idx = str[ctr] - 'a';
-
-		if (crawler->children[idx] == NULL) return;
-
-		crawler = crawler->children[idx];
-		++ctr;
-	}
-
-	if (crawler->isEnd && !crawler->isDel) crawler->isDel = true;
-	return;
 }
 
-
-
-int uniqId = 0;
 int main() {
-	Trie* t = new Trie();
-	t->insert("sing", ++uniqId);
-	t->insert("sip", ++uniqId);
-	t->insert("ask", ++uniqId);
+	LL* list = new LL();
+	list->add("friend");
+	list->add("old");
+	list->add("my");
+	list->add("darkness");
+	list->add("hello");
 
-	printf("%d\n", t->find("sing"));
-	printf("%d\n", t->find("sip"));
-	printf("%d\n", t->find("ask"));
-	printf("%d\n", t->find("sin"));
-	printf("%d\n", t->find("as"));
+	while (!list->empty()) {
+		printf("%s ", list->front()->val);
+		list->remove();
+	}
 	return 0;
 }
