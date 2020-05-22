b799363c89e8a7260a4fcdcafdf8d3f27c56a2e6
pete <petersyjuco@gmail.com>
2020-05-22 22:13:44 +0800

    Daily Kata: Heap

--- Daily Kata/main.cpp
+++ Daily Kata/main.cpp
@@ -1,56 +1,91 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-class DStack {
-	enum { DEF_CAP = 4 };
+struct Node {
+	int val;
+};
+
+class Heap {
+	enum { DEF_CAP = 100 };
 public:
-	DStack(int cap = DEF_CAP);
-	~DStack();
+	Heap(int cap = DEF_CAP);
+	~Heap();
 	int size() const;
 	bool empty() const;
-	int top() const;
+	Node* root() const;
 	void push(const int e);
 	void pop();
 private:
-	int* S;
-	int t;
+	Node** H;
+	int n;
 	int capacity;
 };
+Heap::Heap(int cap) : H(new Node*[cap]), n(0), capacity(cap) { }
+Heap::~Heap() { while (!empty()) pop(); }
+int Heap::size() const { return n; }
+bool Heap::empty() const { return n == 0; }
+Node* Heap::root() const { return empty() ? NULL : H[1]; }
+void Heap::push(const int e) {
+	if (size() != capacity) {
+		Node* v = new Node;
+		v->val = e;
+		++n;
+		H[n] = v;
 
-DStack::DStack(int cap) : S(new int[cap]), t(-1), capacity(cap) { }
-DStack::~DStack() { while (!empty()) pop(); }
-int DStack::size() const { return t + 1; }
-bool DStack::empty() const { return size() == 0; }
-int DStack::top() const { return empty() ? -1 : S[t]; }
-void DStack::push(const int e) {
-	if (size() == capacity) {
-		int* B = new int[capacity * 2];
-		for (int i = 0; i < capacity; ++i) B[i] = S[i];
-		S = B;
-		capacity *= 2;
+		int current = n;
+		while (current > 1 && H[current]->val < H[current / 2]->val) {
+			int parent = current / 2;
+
+			Node* temp = H[parent];
+			H[parent] = H[current];
+			H[current] = temp;
+			current = parent;
+		}
 	}
-	S[++t] = e;
 }
-void DStack::pop() { --t; }
+void Heap::pop() {
+	if (!empty()) {
+		delete H[1];
+		H[1] = H[n];
+		--n;
+
+		int current = 1;
+		while (current * 2 <= n) {
+			int child;
+			int left = current * 2;
+			int right = current * 2 + 1;
+
+			if (left == n) child = left;
+			else child = H[left]->val < H[right]->val ? left : right;
+
+			if (H[current]->val < H[child]->val) break;
+
+			Node* temp = H[child];
+			H[child] = H[current];
+			H[current] = temp;
+			current = child;
+		}
+	}
+}
 
 int main() {
-	DStack* st = new DStack(5);
-	st->push(0);
-	st->push(1);
-	st->push(2);
-	st->push(3);
-	st->push(4);
-	st->push(5);
-	st->push(6);
-	st->push(7);
-	st->push(8);
-	st->push(9);
-	st->push(10);
+	Heap* h = new Heap();
+	h->push(7);
+	h->push(5);
+	h->push(2);
+	h->push(4);
+	h->push(0);
+	h->push(9);
+	h->push(1);
+	h->push(6);
+	h->push(3);
+	h->push(8);
 
-	while (!st->empty()) {
-		printf("%d\n", st->top());
-		st->pop();
+	while (!h->empty()) {
+		printf("%d\n", h->root()->val);
+		h->pop();
 	}
+	printf("FIN\n");
 
 	return 0;
 }
