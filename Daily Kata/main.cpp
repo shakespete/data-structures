805ab0bdcc290d5efd7983688624ede57041a305
pete <petersyjuco@gmail.com>
2020-05-28 21:23:57 +0800

    Daily Kata: Stack

--- Daily Kata/main.cpp
+++ Daily Kata/main.cpp
@@ -1,137 +1,50 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-struct Node {
-	const char* val;
-	Node* prev;
-	Node* next;
-};
-
-class DLL {
-public:
-	DLL();
-	~DLL();
-	bool empty() const;
-	Node* front() const;
-	Node* back() const;
-	void add(Node* v, const char* e);
-	void addFront(const char* e);
-	void addBack(const char* e);
-	void remove(Node* v);
-	void removeFront();
-	void removeBack();
-private:
-	Node* head;
-	Node* tail;
-};
-DLL::DLL() {
-	head = new Node;
-	tail = new Node;
-	head->val = NULL;
-	tail->val = NULL;
-	head->next = tail;
-	tail->prev = head;
-}
-DLL::~DLL() { while (!empty()) removeFront(); }
-bool DLL::empty() const { return head->next == tail; }
-Node* DLL::front() const { return empty() ? NULL : head->next; }
-Node* DLL::back() const { return empty() ? NULL : tail->prev; }
-void DLL::add(Node* v, const char* e) {
-	Node* u = new Node;
-	u->val = e;
-
-	u->prev = v->prev;
-	u->next = v;
-	v->prev->next = u;
-	v->prev = u;
-}
-void DLL::addFront(const char* e) { add(head->next, e); }
-void DLL::addBack(const char* e) { add(tail, e); }
-void DLL::remove(Node* v) {
-	if (!empty()) {
-		Node* u = v->prev;
-		Node* w = v->next;
-		u->next = w;
-		w->prev = u;
-		delete v;
-	}	
-}
-void DLL::removeFront() { if (!empty()) remove(head->next); }
-void DLL::removeBack() { if (!empty()) remove(tail->prev); }
-
-class HashMap {
+class Stack {
 	enum { DEF_CAP = 100 };
 public:
-	HashMap(int cap = DEF_CAP);
-	int hash(const char* e);
-	void insert(const char* e);
-	void remove(const char* e);
-	Node* retrieve(const char* e);
+	Stack(int cap = DEF_CAP);
+	~Stack();
+	int size() const;
+	bool empty() const;
+	int top() const;
+	void push(const int e);
+	void pop();
 private:
-	DLL* HM;
+	int* S;
+	int t;
 	int capacity;
 };
 
-bool is_equal(const char* a, const char* b) {
-	while (*a == *b) {
-		if (*a == '\0') return true;
-		++a;
-		++b;
+Stack::Stack(int cap) : S(new int[cap]), t(-1), capacity(cap) { }
+Stack::~Stack() { while (!empty()) pop(); }
+int Stack::size() const { return t + 1; }
+bool Stack::empty() const { return size() == 0; }
+int Stack::top() const { return empty() ? -1 : S[t]; }
+void Stack::push(const int e) {
+	if (size() != capacity) {
+		S[++t] = e;
 	}
-	return false;
 }
-
-HashMap::HashMap(int cap) : HM(new DLL[cap]), capacity(cap) { }
-int HashMap::hash(const char* e) {
-	int hash = 31;
-	int c;
-	while (c = *e++) {
-		hash = (((hash << 5) + hash) + c) % capacity;
-	}
-	return hash % capacity;
-}
-void HashMap::insert(const char* e) {
-	int hashVal = hash(e);
-	HM[hashVal].addFront(e);
-}
-void HashMap::remove(const char* e) {
-	int hashVal = hash(e);
-	if (HM[hashVal].empty()) return;
-
-	Node* node = retrieve(e);
-	if (node != NULL) HM[hashVal].remove(node);
-	return;
-}
-Node* HashMap::retrieve(const char* e) {
-	int hashVal = hash(e);
-	if (HM[hashVal].empty()) return NULL;
-
-	Node* node = HM[hashVal].front();
-	while (node->val != NULL) {
-		if (is_equal(e, node->val)) return node;
-		node = node->next;
-	}
-	return NULL;
-}
+void Stack::pop() { --t; }
 
 int main() {
-	HashMap* hm = new HashMap();
-	hm->insert("is");
-	hm->insert("misfortune");
-	hm->insert("school");
-	hm->insert("The");
-	hm->insert("good");
-	hm->insert("of");
-	hm->insert("school");
-	hm->insert("a");
+	Stack* st = new Stack();
+	st->push(10);
+	st->push(9);
+	st->push(8);
+	st->push(7);
+	st->push(6);
+	st->push(5);
+	st->push(4);
+	st->push(3);
+	st->push(2);
+	st->push(1);
 
-	printf("%s ", hm->retrieve("The")->val);
-	printf("%s ", hm->retrieve("school")->val);
-	printf("%s ", hm->retrieve("of")->val);
-	printf("%s ", hm->retrieve("misfortune")->val);
-	printf("%s ", hm->retrieve("is")->val);
-	printf("%s ", hm->retrieve("a")->val);
-	printf("%s ", hm->retrieve("good")->val);
-	printf("%s ", hm->retrieve("school")->val);
+	while (!st->empty()) {
+		printf("%d ", st->top());
+		st->pop();
+	}
 	return 0;
 }
