d415a1855e1c73075731d2f8b451eb3783758e21
pete <petersyjuco@gmail.com>
2020-06-23 19:38:22 +0800

    Daily Kata: Queue

--- Daily Kata/main.cpp
+++ Daily Kata/main.cpp
@@ -2,91 +2,93 @@
 #include <stdio.h>
 
 struct Node {
-	int val;
+	const char* val;
+	Node* next;
 };
 
-class Heap {
-	enum { DEF_CAP = 100 };
+class CLL {
 public:
-	Heap(int cap = DEF_CAP);
-	~Heap();
-	int size() const;
+	CLL();
+	~CLL();
 	bool empty() const;
-	Node* root() const;
-	void push(const int e);
-	void pop();
+	Node* front() const;
+	Node* back() const;
+	void advance();
+	void add(const char* e);
+	void remove();
 private:
-	Node** H;
-	int n;
-	int capacity;
+	Node* cursor;
 };
 
-Heap::Heap(int cap) : H(new Node*[cap]), n(0), capacity(cap) { }
-Heap::~Heap() { while (!empty()) pop(); }
-int Heap::size() const { return n; }
-bool Heap::empty() const { return n == 0; }
-Node* Heap::root() const { return empty() ? NULL : H[1]; }
-void Heap::push(const int e) {
-	if (size() != capacity) {
-		Node* v = new Node;
-		v->val = e;
-		++n;
-		H[n] = v;
-
-		int current = n;
-		while (current > 1 && H[current]->val < H[current / 2]->val) {
-			int parent = current / 2;
-
-			Node* temp = H[parent];
-			H[parent] = H[current];
-			H[current] = temp;
-			current = parent;
-		}
+CLL::CLL() : cursor(NULL) { }
+CLL::~CLL() { while (!empty()) remove(); }
+bool CLL::empty() const { return cursor == NULL; }
+Node* CLL::front() const { return empty() ? NULL : cursor->next; }
+Node* CLL::back() const { return empty() ? NULL : cursor; }
+void CLL::advance() { if (!empty()) cursor = cursor->next; }
+void CLL::add(const char* e) {
+	Node* v = new Node;
+	v->val = e;
+	if (empty()) {
+		v->next = v;
+		cursor = v;
+	}
+	else {
+		v->next = cursor->next;
+		cursor->next = v;
 	}
 }
-void Heap::pop() {
+void CLL::remove() {
 	if (!empty()) {
-		delete H[1];
-		H[1] = H[n];
+		Node* old = cursor->next;
+		if (old == cursor) cursor = NULL;
+		else cursor->next = old->next;
+		delete old;
+	}
+}
+
+class Queue {
+public:
+	Queue();
+	~Queue();
+	int size() const;
+	bool empty() const;
+	Node* front() const;
+	void enq(const char* e);
+	void deq();
+private:
+	CLL CL;
+	int n;
+};
+
+Queue::Queue() : CL(), n(0) { }
+Queue::~Queue() { while (!empty()) deq(); }
+int Queue::size() const { return n; }
+bool Queue::empty() const { return n == 0; }
+Node* Queue::front() const { return empty() ? NULL : CL.front(); }
+void Queue::enq(const char* e) {
+	CL.add(e);
+	CL.advance();
+	++n;
+}
+void Queue::deq() {
+	if (!empty()) {
+		CL.remove();
 		--n;
-
-		int current = 1;
-		while (current * 2 <= n) {
-			int child;
-			int left = current * 2;
-			int right = current * 2 + 1;
-
-			if (left == n) child = left;
-			else child = H[left]->val < H[right]->val ? left : right;
-
-			if (H[child]->val > H[current]->val) break;
-
-			Node* temp = H[child];
-			H[child] = H[current];
-			H[current] = temp;
-			current = child;
-		}
 	}
 }
 
 int main() {
-	Heap* h = new Heap();
-	h->push(7);
-	h->push(5);
-	h->push(2);
-	h->push(4);
-	h->push(0);
-	h->push(9);
-	h->push(1);
-	h->push(6);
-	h->push(3);
-	h->push(8);
+	Queue* q = new Queue();
+	q->enq("Hello");
+	q->enq("darkness");
+	q->enq("my");
+	q->enq("old");
+	q->enq("friend");
 
-	while (!h->empty()) {
-		printf("%d\n", h->root()->val);
-		h->pop();
+	while (!q->empty()) {
+		printf("%s ", q->front()->val);
+		q->deq();
 	}
-	printf("FIN\n");
-
 	return 0;
 }
