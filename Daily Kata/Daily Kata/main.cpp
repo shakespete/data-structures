87f71b778fd2f22e5a51fd4af172a9a46a8bda84
shakespete <petersyjuco@gmail.com>
2020-07-08 18:26:04 +0800

    Daily Kata: Linked List

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,57 +1,53 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-class DynamicStack {
-    enum { DEF_CAP = 5 };
-public:
-    DynamicStack(int cap = DEF_CAP);
-    ~DynamicStack();
-    int size() const;
-    bool empty() const;
-    int front() const;
-    void push(const int e);
-    int pop();
-private:
-    int* S;
-    int t;
-    int capacity;
+struct Node {
+    const char* val;
+    Node* next;
 };
 
-DynamicStack::DynamicStack(int cap) : S(new int[cap]), t(-1), capacity(cap) { }
-DynamicStack::~DynamicStack() { while(!empty()) pop(); }
-int DynamicStack::size() const { return t + 1; }
-bool DynamicStack::empty() const { return size() == 0; }
-int DynamicStack::front() const { return empty() ? -1 : S[t]; }
-void DynamicStack::push(const int e) {
-    if (size() == capacity) {
-        int* B = new int[capacity * 2];
-        for (int i = 0; i < capacity; ++i) B[i] = S[i];
-        S = B;
-        capacity *= 2;
-    }
-    S[++t] = e;
-}
-int DynamicStack::pop() { return S[t--]; }
+class LinkedList {
+public:
+    LinkedList();
+    ~LinkedList();
+    bool empty() const;
+    Node* front() const;
+    void add(const char* e);
+    void remove();
+private:
+    Node* head;
+};
 
-int main() {
-    DynamicStack* ds = new DynamicStack();
-    ds->push(1);
-    ds->push(2);
-    ds->push(3);
-    ds->push(4);
-    ds->push(5);
-    ds->push(6);
-    ds->push(7);
-    ds->push(8);
-    ds->push(9);
-    ds->push(10);
-    ds->push(11);
-    ds->push(12);
-    ds->push(13);
-    ds->push(14);
-    ds->push(15);
+LinkedList::LinkedList() : head(NULL) { }
+LinkedList::~LinkedList() { while (!empty()) remove(); }
+bool LinkedList::empty() const { return head == NULL; }
+Node* LinkedList::front() const { return empty() ? NULL : head; }
+void LinkedList::add(const char* e) {
+    Node* v = new Node;
+    v->val = e;
+    v->next = head;
+    head = v;
+}
+void LinkedList::remove() {
+    if (!empty()) {
+        Node* old = head;
+        head = old->next;
+        delete old;
+    }
+}
+
+int main () {
+    LinkedList* ll = new LinkedList();
+    ll->add("friend");
+    ll->add("old");
+    ll->add("my");
+    ll->add("darkness");
+    ll->add("Hello");
     
-    while (!ds->empty()) { printf("%d ", ds->pop()); }
-    printf("\nFIN\n");
+    while (!ll->empty()) {
+        printf("%s ", ll->front()->val);
+        ll->remove();
+    }
+    printf("\n");
     return 0;
 }
