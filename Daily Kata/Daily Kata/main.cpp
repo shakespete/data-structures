900dbc46a74def991315b4a0a6fa5a3732e38cdb
shakespete <petersyjuco@gmail.com>
2020-07-13 17:36:48 +0800

    Daily Kata: Heap

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -2,52 +2,91 @@
 #include <stdio.h>
 
 struct Node {
-    const char* val;
-    Node* next;
+    int val;
 };
 
-class LinkedList {
+class Heap {
+    enum { DEF_CAP = 100 };
 public:
-    LinkedList();
-    ~LinkedList();
+    Heap(int cap = DEF_CAP);
+    ~Heap();
+    int size() const;
     bool empty() const;
-    Node* front() const;
-    void add(const char* e);
-    void remove();
+    Node* root() const;
+    void push(const int e);
+    void pop();
 private:
-    Node* head;
+    Node** H;
+    int n;
+    int capacity;
 };
 
-LinkedList::LinkedList() : head(NULL) { }
-LinkedList::~LinkedList() { while (!empty()) remove(); }
-bool LinkedList::empty() const { return head == NULL; }
-Node* LinkedList::front() const { return empty() ? NULL : head; }
-void LinkedList::add(const char* e) {
-    Node* v = new Node;
-    v->val = e;
-    v->next = head;
-    head = v;
+Heap::Heap(int cap) : H(new Node*[cap]), n(0), capacity(cap) { }
+Heap::~Heap() { while (!empty()) pop(); }
+int Heap::size() const { return n; }
+bool Heap::empty() const { return n == 0; }
+Node* Heap::root() const { return empty() ? NULL : H[1]; }
+void Heap::push(const int e) {
+    if (size() != capacity) {
+        Node* v = new Node;
+        v->val = e;
+        ++n;
+        H[n] = v;
+        
+        int current = n;
+        while (current > 1 && H[current]->val < H[current / 2]->val) {
+            int parent = current / 2;
+            
+            Node* temp = H[parent];
+            H[parent] = H[current];
+            H[current] = temp;
+            current = parent;
+        }
+    }
 }
-void LinkedList::remove() {
+void Heap::pop() {
     if (!empty()) {
-        Node* old = head;
-        head = old->next;
-        delete old;
+        delete H[1];
+        H[1] = H[n];
+        --n;
+        
+        int current = 1;
+        while (current * 2 <= n) {
+            int child;
+            int left = current * 2;
+            int right = current * 2 + 1;
+            
+            if (left == n) child = left;
+            else child = H[left]->val < H[right]->val ? left : right;
+            
+            if (H[child]->val > H[current]->val) break;
+            
+            Node* temp = H[child];
+            H[child] = H[current];
+            H[current] = temp;
+            current = child;
+        }
     }
 }
 
-int main () {
-    LinkedList* ll = new LinkedList();
-    ll->add("friend");
-    ll->add("old");
-    ll->add("my");
-    ll->add("darkness");
-    ll->add("Hello");
-    
-    while (!ll->empty()) {
-        printf("%s ", ll->front()->val);
-        ll->remove();
+int main() {
+    Heap* h = new Heap();
+    h->push(7);
+    h->push(5);
+    h->push(2);
+    h->push(4);
+    h->push(0);
+    h->push(9);
+    h->push(1);
+    h->push(6);
+    h->push(3);
+    h->push(8);
+
+    while (!h->empty()) {
+        printf("%d\n", h->root()->val);
+        h->pop();
     }
-    printf("\n");
+    printf("FIN\n");
+
     return 0;
 }
