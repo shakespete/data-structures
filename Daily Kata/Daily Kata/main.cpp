d908e1bcc0aa0b3ba1561d38eea4712b5c1ac9cf
shakespete <petersyjuco@gmail.com>
2020-07-23 21:22:16 +0800

    Daily Kata: Linked List

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,60 +1,52 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-class Stack {
-    enum { DEF_CAP = 5 };
-public:
-    Stack(int cap = DEF_CAP);
-    ~Stack();
-    int size() const;
-    bool empty() const;
-    int top() const;
-    void push(const int e);
-    void pop();
-private:
-    int* S;
-    int t;
-    int capacity;
+struct Node {
+    const char* val;
+    Node* next;
 };
 
-Stack::Stack(int cap) : S(new int[cap]), t(-1), capacity(cap) { }
-Stack::~Stack() { while (!empty()) pop(); }
-int Stack::size() const { return t + 1;}
-bool Stack::empty() const { return size() == 0; }
-int Stack::top() const { return empty() ? -1 : S[t]; }
-void Stack::push(const int e) {
-    if (size() == capacity) {
-        int* B = new int[capacity * 2];
-        for (int i = 0; i < capacity; ++i) B[i] = S[i];
-        S = B;
-        delete[] B;
-    }
-    ++t;
-    S[t] = e;
+class LL {
+public:
+    LL();
+    ~LL();
+    bool empty() const;
+    Node* front() const;
+    void add(const char* e);
+    void remove();
+private:
+    Node* head;
+};
+
+LL::LL() : head(NULL) { }
+LL::~LL() { while (!empty()) remove(); }
+bool LL::empty() const { return head == NULL; }
+Node* LL::front() const { return empty() ? NULL : head; }
+void LL::add(const char* e) {
+    Node* v = new Node;
+    v->val = e;
+    v->next = head;
+    head = v;
 }
-void Stack::pop() { if (!empty()) --t; }
+void LL::remove() {
+    if (!empty()) {
+        Node* old = head;
+        head = old->next;
+        delete old;
+    }
+}
 
 int main() {
-    Stack* st = new Stack();
-    st->push(15);
-    st->push(14);
-    st->push(13);
-    st->push(12);
-    st->push(11);
-    st->push(10);
-    st->push(9);
-    st->push(8);
-    st->push(7);
-    st->push(6);
-    st->push(5);
-    st->push(4);
-    st->push(3);
-    st->push(2);
-    st->push(1);
+    LL* list = new LL();
+    list->add("friend");
+    list->add("old");
+    list->add("my");
+    list->add("darkness");
+    list->add("hello");
     
-    while (!st->empty()) {
-        printf("%d ", st->top());
-        st->pop();
+    while (!list->empty()) {
+        printf("%s ", list->front()->val);
+        list->remove();
     }
     printf("\n");
     
