d968ee0ffbbfc93e51490b1270090d9b0956416d
shakespete <petersyjuco@gmail.com>
2020-07-29 19:50:34 +0800

    Daily Kata: Hash Map

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,107 +1,137 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-class TrieNode {
-public:
-    TrieNode();
-    int key;
-    bool isEnd;
-    bool isDel;
-    TrieNode** children;
+struct Node {
+    const char* val;
+    Node* prev;
+    Node* next;
 };
-TrieNode::TrieNode () {
-    key = -1;
-    isEnd = false;
-    isDel = false;
+
+class DLL {
+public:
+    DLL();
+    ~DLL();
+    bool empty() const;
+    Node* front() const;
+    Node* back() const;
+    void add(Node* v, const char* e);
+    void addFront(const char* e);
+    void addBack(const char* e);
+    void remove(Node* v);
+    void removeFront();
+    void removeBack();
+private:
+    Node* head;
+    Node* tail;
+};
+
+DLL::DLL() {
+    head = new Node;
+    tail = new Node;
+    head->next = tail;
+    tail->prev = head;
+    head->val = NULL;
+    tail->val = NULL;
+}
+DLL::~DLL() { while (!empty()) removeFront(); }
+bool DLL::empty() const { return head->next == tail; }
+Node* DLL::front() const { return empty() ? NULL : head->next; }
+Node* DLL::back() const { return empty() ? NULL : tail->prev; }
+void DLL::add(Node* v, const char* e) {
+    Node* u = new Node;
+    u->val = e;
     
-    children = new TrieNode*[26];
-    for (int i = 0; i < 26; ++i) {
-        children[i] = NULL;
+    u->next = v;
+    u->prev = v->prev;
+    v->prev->next = u;
+    v->prev = u;
+}
+void DLL::addFront(const char* e) { add(head->next, e); }
+void DLL::addBack(const char* e) {add(tail, e); }
+void DLL::remove(Node* v) {
+    if (!empty()) {
+        Node* u = v->prev;
+        Node* w = v->next;
+        u->next = w;
+        w->prev = u;
+        delete v;
     }
+}
+void DLL::removeFront() { if (!empty()) remove(head->next); }
+void DLL::removeBack() { if (!empty()) remove(tail->prev); }
+
+
+
+class HashMap {
+    enum { DEF_CAP = 100 };
+public:
+    HashMap(int cap = DEF_CAP);
+    int hash(const char* e);
+    void insert(const char* e);
+    void remove(const char* e);
+    Node* retrieve(const char* e);
+private:
+    DLL* HM;
+    int capacity;
+};
+
+
+bool is_equal(const char* a, const char* b) {
+    while (*a == *b) {
+        if (*a == '\0') return true;
+        ++a;
+        ++b;
+    }
+    return false;
 }
 
-class Trie {
-public:
-    Trie();
-    void insert(const char* str, const int k);
-    int find(const char* str);
-    void remove(const char* str);
-private:
-    TrieNode* root;
-};
-Trie::Trie() { root = new TrieNode(); }
-void Trie::insert(const char* str, const int k) {
-    TrieNode* crawler = root;
-    
-    int ctr = 0;
-    int idx;
-    while (str[ctr] != '\0') {
-        idx = str[ctr] - 'a';
-        
-        if (crawler->children[idx] == NULL)
-            crawler->children[idx] = new TrieNode();
-        
-        crawler = crawler->children[idx];
-        ++ctr;
+HashMap::HashMap(int cap) : HM(new DLL[cap]), capacity(cap) { }
+int HashMap::hash(const char* e) {
+    int hash = 31;
+    int c;
+    while (c = *e++) {
+        hash = (((hash << 5) + hash) + c) % capacity;
     }
-    
-    crawler->isEnd = true;
-    crawler->key = k;
+    return hash % capacity;
 }
-int Trie::find(const char* str) {
-    TrieNode* crawler = root;
-    
-    int ctr = 0;
-    int idx;
-    while (str[ctr] != '\0') {
-        idx = str[ctr] - 'a';
-        
-        if (crawler->children[idx] == NULL) return -1;
-        
-        crawler = crawler->children[idx];
-        ++ctr;
-    }
-    
-    if (crawler->isEnd && !crawler->isDel) return crawler->key;
-    return -1;
+void HashMap::insert(const char* e) {
+    int hashVal = hash(e);
+    HM[hashVal].addBack(e);
 }
-void Trie::remove(const char* str) {
-    TrieNode* crawler = root;
+void HashMap::remove(const char* e) {
+    int hashVal = hash(e);
+    if (HM[hashVal].empty()) return;
     
-    int ctr = 0;
-    int idx;
-    while (str[ctr] != '\0') {
-        idx = str[ctr] - 'a';
-        
-        if (crawler->children[idx] == NULL) return;
-        
-        crawler = crawler->children[idx];
-        ++ctr;
-    }
-    
-    if (crawler->isEnd) crawler->isDel = true;
+    Node* node = retrieve(e);
+    if (node != NULL) HM[hashVal].remove(node);
     return;
 }
+Node* HashMap::retrieve(const char* e) {
+    int hashVal = hash(e);
+    if (HM[hashVal].empty()) return NULL;
+    
+    Node* node = HM[hashVal].front();
+    while (node->val != NULL) {
+        if (is_equal(e, node->val)) return node;
+        node = node->next;
+    }
+    return NULL;
+}
 
-int uniq_id = 0;
 int main() {
-    Trie* t = new Trie();
-    t->insert("hello", ++uniq_id);
-    t->insert("darkness", ++uniq_id);
-    t->insert("my", ++uniq_id);
-    t->insert("old", ++uniq_id);
-    t->insert("friend", ++uniq_id);
+    HashMap* hm = new HashMap();
+    hm->insert("friend");
+    hm->insert("darkness");
+    hm->insert("old");
+    hm->insert("my");
+    hm->insert("Hello");
     
-    printf("%d\n", t->find("hello"));
-    printf("%d\n", t->find("darkness"));
-    printf("%d\n", t->find("my"));
-    printf("%d\n", t->find("old"));
-    printf("%d\n", t->find("friend"));
+    printf("%s ", hm->retrieve("Hello")->val);
+    printf("%s ", hm->retrieve("darkness")->val);
+    printf("%s ", hm->retrieve("my")->val);
+    printf("%s ", hm->retrieve("old")->val);
+    printf("%s ", hm->retrieve("friend")->val);
+    printf("\n");
     
-    t->remove("hello");
-    t->remove("friend");
-    
-    printf("%d\n", t->find("hello"));
-    printf("%d\n", t->find("friend"));
     return 0;
 }
