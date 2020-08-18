2dda482b9fbc3489f7a020f93004bcec841e17ac
shakespete <petersyjuco@gmail.com>
2020-08-18 20:30:36 +0800

    Daily Kata: Dynamic Stack

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -2,88 +2,69 @@
 #include <stdio.h>
 
 struct Node {
-    int val;
+    const char* val;
 };
 
-class Heap {
-    enum { DEF_CAP = 50 };
+class Stack {
+    enum { DEF_CAP = 5 };
 public:
-    Heap(int cap = DEF_CAP);
-    ~Heap();
+    Stack(int cap = DEF_CAP);
+    ~Stack();
     int size() const;
     bool empty() const;
-    Node* root() const;
-    void push(const int e);
+    Node* top() const;
+    void push(const char* e);
     void pop();
 private:
-    Node** H;
-    int n;
+    Node** S;
+    int t;
     int capacity;
 };
 
-Heap::Heap(int cap) : H(new Node*[cap]), n(0), capacity(cap) { }
-Heap::~Heap() { while (!empty()) pop(); }
-int Heap::size() const { return n; }
-bool Heap::empty() const { return n == 0; }
-Node* Heap::root() const { return empty() ? NULL : H[1];}
-void Heap::push(const int e) {
-    if (size() != capacity) {
-        Node* v = new Node();
-        v->val = e;
-        ++n;
-        H[n] = v;
-        
-        int current = n;
-        while (current > 1 && H[current]->val < H[current / 2]->val) {
-            int parent = current / 2;
-            
-            Node* temp = H[parent];
-            H[parent] = H[current];
-            H[current] = temp;
-            current = parent;
-        }
+Stack::Stack(int cap) : S(new Node*[cap]), t(-1), capacity(cap) { }
+Stack::~Stack() { while (!empty()) pop(); }
+int Stack::size() const { return t + 1;}
+bool Stack::empty() const { return size() == 0; }
+Node* Stack::top() const { return empty() ? NULL : S[t]; }
+void Stack::push(const char* e) {
+    Node* v = new Node;
+    v->val = e;
+    if (size() == capacity) {
+        Node** B = new Node*[capacity * 2];
+        for (int i = 0; i < capacity; ++i) B[i] = S[i];
+        S = B;
+        capacity *= 2;
     }
+    ++t;
+    S[t] = v;
 }
-void Heap::pop() {
-    if (!empty()) {
-        delete H[1];
-        H[1] = H[n];
-        --n;
-        
-        int current = 1;
-        while (current * 2 <= n) {
-            int child;
-            int left = current * 2;
-            int right = current * 2 + 1;
-            
-            if (left == n) child = left;
-            else child = H[left]->val < H[right]->val ? left : right;
-            
-            if (H[child]->val > H[current]->val) break;
-            
-            Node* temp = H[child];
-            H[child] = H[current];
-            H[current] = temp;
-            current = child;
-        }
-    }
+void Stack::pop() {
+    if (!empty()) delete S[t];
+    --t;
 }
 
 int main() {
-    Heap* hp = new Heap();
-    hp->push(8);
-    hp->push(2);
-    hp->push(0);
-    hp->push(7);
-    hp->push(9);
-    hp->push(6);
-    hp->push(1);
-    hp->push(5);
-    hp->push(3);
+    Stack* st = new Stack();
+    st->push("silence");
+    st->push("in");
+    st->push("be");
+    st->push("may");
+    st->push("there");
+    st->push("peace");
+    st->push("what");
+    st->push("remember");
+    st->push("and");
+    st->push("haste");
+    st->push("and");
+    st->push("noise");
+    st->push("the");
+    st->push("amid");
+    st->push("placidly");
+    st->push("Go");
     
-    while (!hp->empty()) {
-        printf("%d ", hp->root()->val);
-        hp->pop();
+    while (!st->empty()) {
+        printf("%s ", st->top()->val);
+        st->pop();
     }
     printf("\n");
     return 0;
