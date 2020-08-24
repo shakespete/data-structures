3932bee91aeef6235d682c05b7c201e452a15b52
shakespete <petersyjuco@gmail.com>
2020-08-24 17:54:36 +1000

    Daily Kata: Stack

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -3,50 +3,65 @@
 
 struct Node {
     const char* val;
-    Node* next;
 };
 
-class LL {
+class Stack {
+    enum { DEF_CAP = 2 };
 public:
-    LL();
-    ~LL();
+    Stack(int cap = DEF_CAP);
+    ~Stack();
+    int size() const;
     bool empty() const;
-    Node* front() const;
-    void add(const char* e);
-    void remove();
+    Node* top() const;
+    void push(const char* e);
+    void pop();
 private:
-    Node* head;
+    Node** S;
+    int t;
+    int capacity;
 };
 
-LL::LL() : head(NULL) { }
-LL::~LL() { while (!empty()) remove(); }
-bool LL::empty() const { return head == NULL; }
-Node* LL::front() const { return empty() ? NULL : head; }
-void LL::add(const char* e) {
+Stack::Stack(int cap) : S(new Node*[cap]), t(-1), capacity(cap) { }
+Stack::~Stack() { while (!empty()) pop(); }
+int Stack::size() const { return t + 1; }
+bool Stack::empty() const { return size() == 0; }
+Node* Stack::top() const { return empty() ? NULL : S[t]; }
+void Stack::push(const char* e) {
     Node* v = new Node;
     v->val = e;
-    v->next = head;
-    head = v;
+    if (size() == capacity) {
+        Node** B = new Node*[capacity * 2];
+        
+        for (int i = 0; i < capacity; ++i) B[i] = S[i];
+        
+        S = B;
+        capacity *= 2;
+    }
+    ++t;
+    S[t] = v;
 }
-void LL::remove() {
+void Stack::pop() {
     if (!empty()) {
-        Node* old = head;
-        head = old->next;
-        delete old;
+        delete S[t];
+        --t;
     }
 }
 
 int main() {
-    LL* list = new LL();
-    list->add("friend");
-    list->add("old");
-    list->add("my");
-    list->add("darkness");
-    list->add("Hello");
+    Stack* st = new Stack();
+    st->push(".");
+    st->push("waste");
+    st->push("to");
+    st->push("thing");
+    st->push("terrible");
+    st->push("a");
+    st->push("is");
+    st->push("mind");
+    st->push("The");
     
-    while (!list->empty()) {
-        printf("%s ", list->front()->val);
-        list->remove();
+    while (!st->empty()) {
+        printf("%s ", st->top()->val);
+        st->pop();
     }
     printf("\n");
     return 0;
