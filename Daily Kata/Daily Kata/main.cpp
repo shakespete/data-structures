80354245d5c4d59badd32160a157ff8dd8ee345f
shakespete <petersyjuco@gmail.com>
2020-08-31 13:33:36 +1000

    Daily Kata: Hash Map

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -3,66 +3,131 @@
 
 struct Node {
     const char* val;
+    Node* prev;
+    Node* next;
 };
 
-class Stack {
-    enum { DEF_CAP = 2 };
+class DLL {
 public:
-    Stack(int cap = DEF_CAP);
-    ~Stack();
-    int size() const;
+    DLL();
+    ~DLL();
     bool empty() const;
-    Node* top() const;
-    void push(const char* e);
-    void pop();
+    Node* front() const;
+    Node* back() const;
+    void add(Node* v, const char* e);
+    void addFront(const char* e);
+    void addBack(const char* e);
+    void remove(Node* v);
+    void removeFront();
+    void removeBack();
 private:
-    Node** S;
-    int t;
+    Node* head;
+    Node* tail;
+};
+
+DLL::DLL() {
+    head = new Node;
+    tail = new Node;
+    head->next = tail;
+    tail->prev = head;
+    head->val = NULL;
+    tail->val = NULL;
+}
+DLL::~DLL() { while (!empty()) removeFront(); }
+bool DLL::empty() const { return head->next == tail; }
+Node* DLL::front() const { return empty() ? NULL : head->next; }
+Node* DLL::back() const {return empty() ? NULL : tail->prev; }
+void DLL::add(Node* v, const char* e) {
+    Node* u = new Node;
+    u->val = e;
+    u->prev = v->prev;
+    u->next = v;
+    v->prev->next = u;
+    v->prev = u;
+}
+void DLL::addFront(const char* e) { add(head->next, e); }
+void DLL::addBack(const char* e) { add(tail, e); }
+void DLL::remove(Node* v) {
+    if (!empty()) {
+        Node* u = v->prev;
+        Node* w = v->next;
+        u->next = w;
+        w->prev = u;
+        delete v;
+    }
+}
+void DLL::removeFront() { if (!empty()) remove(head->next); }
+void DLL::removeBack() { if (!empty()) remove(tail->prev); }
+
+
+bool is_equal(const char* a, const char* b) {
+    while (*a == *b) {
+        if (*a == '\0') return true;
+        ++a;
+        ++b;
+    }
+    return false;
+}
+
+class HashMap {
+    enum { DEF_CAP = 100 };
+public:
+    HashMap(int cap = DEF_CAP);
+    int hash(const char* e);
+    void insert(const char* e);
+    void remove(const char* e);
+    Node* retrieve(const char* e);
+private:
+    DLL* HM;
     int capacity;
 };
 
-Stack::Stack(int cap) : S(new Node*[cap]), t(-1), capacity(cap) { }
-Stack::~Stack() { while (!empty()) pop(); }
-int Stack::size() const { return t + 1; }
-bool Stack::empty() const { return size() == 0; }
-Node* Stack::top() const { return empty() ? NULL : S[t]; }
-void Stack::push(const char* e) {
-    Node* v = new Node;
-    v->val = e;
-    if (size() == capacity) {
-        Node** B = new Node*[capacity * 2];
-        
-        for (int i = 0; i < capacity; ++i) B[i] = S[i];
-        
-        S = B;
-        capacity *= 2;
+HashMap::HashMap(int cap) : HM(new DLL[cap]), capacity(cap) { }
+int HashMap::hash(const char* e) {
+    int hash = 31;
+    int c;
+    while (c = *e++) {
+        hash = (((hash << 5) + hash) + c) % capacity;
     }
-    ++t;
-    S[t] = v;
+    return hash % capacity;
 }
-void Stack::pop() {
-    if (!empty()) {
-        delete S[t];
-        --t;
+void HashMap::insert(const char* e) {
+    int hashVal = hash(e);
+    HM[hashVal].addBack(e);
+}
+void HashMap::remove(const char* e) {
+    int hashVal = hash(e);
+    if (HM[hashVal].empty()) return;
+    
+    Node* node = retrieve(e);
+    if (node != NULL) HM[hashVal].remove(node);
+    return;
+}
+Node* HashMap::retrieve(const char* e) {
+    int hashVal = hash(e);
+    if (HM[hashVal].empty()) return NULL;
+    
+    Node* node = HM[hashVal].front();
+    while (node->val != NULL) {
+        if (is_equal(e, node->val)) return node;
+        node = node->next;
     }
+    return NULL;
 }
 
 int main() {
-    Stack* st = new Stack();
-    st->push(".");
-    st->push("waste");
-    st->push("to");
-    st->push("thing");
-    st->push("terrible");
-    st->push("a");
-    st->push("is");
-    st->push("mind");
-    st->push("The");
+    HashMap* hm = new HashMap();
+    hm->insert("friend");
+    hm->insert("my");
+    hm->insert("old");
+    hm->insert("darkness");
+    hm->insert("Hello");
     
-    while (!st->empty()) {
-        printf("%s ", st->top()->val);
-        st->pop();
-    }
+    printf("%s ", hm->retrieve("Hello")->val);
+    printf("%s ", hm->retrieve("darkness")->val);
+    printf("%s ", hm->retrieve("my")->val);
+    printf("%s ", hm->retrieve("old")->val);
+    printf("%s ", hm->retrieve("friend")->val);
     printf("\n");
     return 0;
 }
