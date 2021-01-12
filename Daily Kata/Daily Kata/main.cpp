ed6d6dc9fc31b18c3742c2080f6e9d3a72ae6a97
shakespete <petersyjuco@gmail.com>
2021-01-12 12:07:00 +1100

    Daily Kata: Dynamic String Array

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,92 +1,71 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+bool is_equal(const char* a, const char* b) {
+    while (*a == *b) {
+        if (*a == '\0') return true;
+        ++a;
+        ++b;
+    }
+    return false;
+}
+
 struct Node {
-    int val;
+    const char* val;
 };
 
-class Heap {
-    enum { DEF_CAP = 100 };
+class DArray {
+    enum {DEF_CAP = 2};
 public:
-    Heap(int cap = DEF_CAP);
-    ~Heap();
+    DArray(int e = DEF_CAP);
     int size() const;
     bool empty() const;
-    Node* root() const;
-    void push(const int& e);
-    void pop();
+    int find(const char* e);
+    void push(const char* e);
+    const char* pop();
 private:
-    Node** H;
-    int n;
+    Node** A;
     int capacity;
+    int n;
 };
 
-Heap::Heap(int cap) : H(new Node*[cap]), n(0), capacity(cap) { }
-Heap::~Heap() { while (!empty()) pop(); }
-int Heap::size() const { return n; }
-bool Heap::empty() const { return n == 0;}
-Node* Heap::root() const { return empty() ? NULL : H[1]; }
-void Heap::push(const int& e) {
-    if (size() != capacity) {
-        Node* v = new Node;
-        v->val = e;
-        n++;
-        H[n] = v;
-        
-        int current = n;
-        while (current > 1 && H[current]->val < H[current / 2]->val) {
-            int parent = current / 2;
-            
-            Node* temp = H[parent];
-            H[parent] = H[current];
-            H[current] = temp;
-            current = parent;
-        }
+DArray::DArray(int e) : capacity(e), A(new Node*[e]), n(0) { }
+int DArray::size() const { return n; }
+bool DArray::empty() const {return n == 0;};
+int DArray::find(const char* e) {
+    if (empty()) return -1;
+    for (int i = 0; i < n; ++i) {
+        if (is_equal(e, A[i]->val)) return i;
     }
+    return false;
 }
-void Heap::pop() {
-    if (!empty()) {
-        delete H[1];
-        H[1] = H[n];
-        --n;
-        
-        int current = 1;
-        while (current * 2 <= n) {
-            int child;
-            int left = current * 2;
-            int right = current * 2 + 1;
-            
-            if (left == n) child = left;
-            else child = H[left]->val < H[right]->val ? left : right;
-            
-            if (H[current]->val < H[child]->val) break;
-            
-            Node* temp = H[child];
-            H[child] = H[current];
-            H[current] = temp;
-            current = child;
-        }
+void DArray::push(const char* e) {
+    if (size() == capacity) {
+        Node** B = new Node*[capacity * 2];
+        for (int i = 0; i < capacity; ++i) B[i] = A[i];
+        A = B;
+        capacity *= 2;
     }
+    Node* v = new Node;
+    v->val = e;
+    A[n++] = v;
+}
+const char* DArray::pop() {
+    Node* v = A[--n];
+    return v->val;
 }
 
 int main() {
-    Heap* h = new Heap();
-    h->push(7);
-    h->push(5);
-    h->push(2);
-    h->push(4);
-    h->push(0);
-    h->push(9);
-    h->push(1);
-    h->push(6);
-    h->push(3);
-    h->push(8);
-
-    while (!h->empty()) {
-        printf("%d\n", h->root()->val);
-        h->pop();
+    DArray* arr = new DArray();
+    arr->push("friend");
+    arr->push("old");
+    arr->push("my");
+    arr->push("darkness");
+    arr->push("Hello");
+    while (!arr->empty()) {
+        printf("%s ", arr->pop());
     }
-    printf("FIN\n");
-
+    printf("\nFIN\n");
+    
     return 0;
 }
