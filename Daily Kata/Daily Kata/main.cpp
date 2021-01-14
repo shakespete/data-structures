2d6c17d42403084a5472770ce251dbe9b19f2859
shakespete <petersyjuco@gmail.com>
2021-01-14 16:40:16 +1100

    Daily Kata: Trie

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,71 +1,96 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-bool is_equal(const char* a, const char* b) {
-    while (*a == *b) {
-        if (*a == '\0') return true;
-        ++a;
-        ++b;
-    }
-    return false;
-}
-
-struct Node {
-    const char* val;
-};
-
-class DArray {
-    enum {DEF_CAP = 2};
+class TrieNode {
 public:
-    DArray(int e = DEF_CAP);
-    int size() const;
-    bool empty() const;
-    int find(const char* e);
-    void push(const char* e);
-    const char* pop();
-private:
-    Node** A;
-    int capacity;
-    int n;
+    TrieNode();
+    int key;
+    bool isEnd;
+    bool isDel;
+    TrieNode** children;
 };
-
-DArray::DArray(int e) : capacity(e), A(new Node*[e]), n(0) { }
-int DArray::size() const { return n; }
-bool DArray::empty() const {return n == 0;};
-int DArray::find(const char* e) {
-    if (empty()) return -1;
-    for (int i = 0; i < n; ++i) {
-        if (is_equal(e, A[i]->val)) return i;
+TrieNode::TrieNode() {
+    key = 0;
+    isEnd = false;
+    isDel = false;
+    children = new TrieNode*[26];
+    for (int i = 0; i < 26; ++i) {
+        children[i] = NULL;
     }
-    return false;
-}
-void DArray::push(const char* e) {
-    if (size() == capacity) {
-        Node** B = new Node*[capacity * 2];
-        for (int i = 0; i < capacity; ++i) B[i] = A[i];
-        A = B;
-        capacity *= 2;
-    }
-    Node* v = new Node;
-    v->val = e;
-    A[n++] = v;
-}
-const char* DArray::pop() {
-    Node* v = A[--n];
-    return v->val;
 }
 
-int main() {
-    DArray* arr = new DArray();
-    arr->push("friend");
-    arr->push("old");
-    arr->push("my");
-    arr->push("darkness");
-    arr->push("Hello");
-    while (!arr->empty()) {
-        printf("%s ", arr->pop());
-    }
-    printf("\nFIN\n");
+class Trie {
+public:
+    Trie();
+    void insert(const char* str, int k);
+    int find(const char* str);
+    void remove(const char* str);
+    TrieNode* root;
+};
+Trie::Trie() { root = new TrieNode(); }
+void Trie::insert(const char* str, int k) {
+    TrieNode* crawler = root;
     
+    int ctr = 0;
+    int idx;
+    while(str[ctr] != '\0') {
+        idx = str[ctr] - 'a';
+        
+        if (crawler->children[idx] == NULL)
+            crawler->children[idx] = new TrieNode();
+    
+        crawler = crawler->children[idx];
+        ctr++;
+    }
+    crawler->isEnd = true;
+    crawler->key = k;
+}
+int Trie::find(const char* str) {
+    TrieNode* crawler = root;
+    
+    int ctr = 0;
+    int idx;
+    while(str[ctr] != '\0') {
+        idx = str[ctr] - 'a';
+        
+        if (crawler->children[idx] == NULL) return -1;
+    
+        crawler = crawler->children[idx];
+        ctr++;
+    }
+    
+    if (crawler->isEnd) return crawler->key;
+    return -1;
+}
+void Trie::remove(const char* str){
+    TrieNode* crawler = root;
+    
+    int ctr = 0;
+    int idx;
+    while(str[ctr] != '\0') {
+        idx = str[ctr] - 'a';
+        
+        if (crawler->children[idx] == NULL) return;
+    
+        crawler = crawler->children[idx];
+        ctr++;
+    }
+    
+    if (crawler->isEnd) crawler->isDel = true;
+    return;
+}
+
+int uniqId = 0;
+int main() {
+    Trie* t = new Trie();
+    t->insert("sing", ++uniqId);
+    t->insert("sip", ++uniqId);
+    t->insert("ask", ++uniqId);
+
+    printf("%d\n", t->find("sing"));
+    printf("%d\n", t->find("sip"));
+    printf("%d\n", t->find("ask"));
+    printf("%d\n", t->find("sin"));
+    printf("%d\n", t->find("as"));
     return 0;
 }
