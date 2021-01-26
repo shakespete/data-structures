4f145b7d1f66c672bc66133acc0112ad5e70f3e4
shakespete <petersyjuco@gmail.com>
2021-01-27 09:19:21 +1100

    Daily Kata: Doubly Linked List

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -3,52 +3,80 @@
 
 struct Node {
     const char* val;
+    Node* prev;
     Node* next;
 };
 
-class LinkedList {
+class DLL {
 public:
-    LinkedList();
-    ~LinkedList();
+    DLL();
+    ~DLL();
     bool empty() const;
     Node* front() const;
-    void add(const char* e);
-    void remove();
+    Node* back() const;
+    void add(Node* v, const char* e);
+    void addFront(const char* e);
+    void addBack(const char* e);
+    void remove(Node* v);
+    void removeFront();
+    void removeBack();
 private:
     Node* head;
+    Node* tail;
 };
 
-LinkedList::LinkedList() : head(NULL) {}
-LinkedList::~LinkedList() { while (!empty()) remove(); }
-bool LinkedList::empty() const { return head == NULL; }
-Node* LinkedList::front() const { return empty() ? NULL : head; }
-void LinkedList::add(const char* e) {
-    Node* v = new Node;
-    v->val = e;
-    v->next = head;
-    head = v;
+DLL::DLL() {
+    head = new Node;
+    tail = new Node;
+    head->val = NULL;
+    tail->val = NULL;
+    head->next = tail;
+    tail->prev = head;
 }
-void LinkedList::remove() {
+DLL::~DLL() { while (!empty()) removeFront(); }
+bool DLL::empty() const { return head->next == tail; }
+Node* DLL::front() const { return empty() ? NULL : head->next; }
+Node* DLL::back() const { return empty() ? NULL : tail->prev; }
+void DLL::add(Node* v, const char* e) {
+    Node* u = new Node;
+    u->val = e;
+    u->prev = v->prev;
+    u->next = v;
+    v->prev->next = u;
+    v->prev = u;
+    
+}
+void DLL::addFront(const char* e) { add(head->next, e); }
+void DLL::addBack(const char* e) { add(tail, e); }
+void DLL::remove(Node* v) {
     if (!empty()) {
-        Node* old = head;
-        head = old->next;
-        delete old;
+        Node* u = v->prev;
+        Node* w = v->next;
+        u->next = w;
+        w->prev = u;
+        delete v;
     }
 }
+void DLL::removeFront() { remove(head->next); }
+void DLL::removeBack() {remove(tail->prev); }
 
 int main() {
-    LinkedList* LL = new LinkedList();
-    LL->add("friend");
-    LL->add("old");
-    LL->add("my");
-    LL->add("darkness");
-    LL->add("Hello");
-    
-    while (!LL->empty()) {
-        printf("%s ", LL->front()->val);
-        LL->remove();
+    DLL* dList = new DLL();
+    dList->addBack("Do");
+    dList->addBack("not");
+    dList->addBack("go");
+    dList->addBack("gentle");
+    dList->addBack("into");
+    dList->addBack("that");
+    dList->addBack("good");
+    dList->addBack("night");
+
+    Node* node = dList->front();
+    while (node->val) {
+        printf("%s ", node->val);
+        node = node->next;
     }
     printf("\nFIN\n");
-    
+
     return 0;
 }
