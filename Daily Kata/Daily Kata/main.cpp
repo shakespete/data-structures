3798827c34ea84cc52737676af234e22e29a1cef
shakespete <petersyjuco@gmail.com>
2021-02-01 08:49:42 +1100

    Daily Kata: Circular Linked List

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,79 +1,68 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-bool is_equal(const char* a, const char* b) {
-    while (*a == *b) {
-        if (*a == '\0') return true;
-        ++a;
-        ++b;
-    }
-    return false;
-}
-
 struct Node {
     const char* val;
+    Node* next;
 };
 
-class DArray {
-    enum { DEF_CAP = 2 };
+class CLL {
 public:
-    DArray(int cap = DEF_CAP);
-    ~DArray();
-    int size() const;
+    CLL();
+    ~CLL();
     bool empty() const;
-    int find(const char* e);
-    void push(const char* e);
-    const char* pop();
+    Node* front() const;
+    Node* back() const;
+    void advance();
+    void add(const char* e);
+    void remove();
 private:
-    Node** A;
-    int capacity;
-    int n;
+    Node* cursor;
 };
-DArray::DArray(int cap) : A(new Node*[cap]), capacity(cap), n(0) {}
-DArray::~DArray() { while (!empty()) pop(); }
-int DArray::size() const { return n; }
-bool DArray::empty() const { return n == 0; }
-int DArray::find(const char* e) {
-    if (empty()) return -1;
-    
-    for (int i = 0; i < n; ++i)
-        if (is_equal(e, A[i]->val))
-            return i;
-    return -1;
-}
-void DArray::push(const char* e) {
-    if (size() == capacity) {
-        Node** B = new Node*[capacity * 2];
-        for (int i = 0; i < capacity; ++i) B[i] = A[i];
-        A = B;
-        capacity *= 2;
-    }
+
+CLL::CLL() : cursor(NULL) { }
+CLL::~CLL() { while (!empty()) remove(); }
+bool CLL::empty() const { return  cursor == NULL; }
+Node* CLL::front() const { return empty() ? NULL : cursor->next; }
+Node* CLL::back() const { return empty() ? NULL : cursor; }
+void CLL::advance() { if (!empty()) cursor = cursor->next; }
+void CLL::add(const char* e) {
     Node* v = new Node;
     v->val = e;
-    A[n++] = v;
+    if (empty()) {
+        v->next = v;
+        cursor = v;
+    } else {
+        v->next = cursor->next;
+        cursor->next = v;
+    }
 }
-const char* DArray::pop() {
-    Node* v = A[--n];
-    return v->val;
+void CLL::remove() {
+    if (!empty()) {
+        Node* old = cursor->next;
+        if (old == cursor) cursor = NULL;
+        else cursor->next = old->next;
+        delete old;
+    }
 }
-
 
 int main() {
-    DArray* arr = new DArray();
-    arr->push("friend");
-    arr->push("old");
-    arr->push("my");
-    arr->push("darkness");
-    arr->push("Hello");
-    
-    printf("%d\n", arr->find("old"));
-    printf("%d\n", arr->find("darkness"));
-    printf("%d\n", arr->find("ununravelable"));
-    
-    while (!arr->empty()) {
-        printf("%s ", arr->pop());
+    CLL* clist = new CLL();
+    clist->add("old");
+    clist->add("my");
+    clist->add("darkness");
+    clist->add("Hello");
+    clist->add("friend");
+
+    clist->advance();
+
+    Node* node = clist->front();
+    while (node) {
+        printf("%s ", node->val);
+        clist->remove();
+        node = clist->front();
     }
-    
     printf("\nFIN\n");
+
     return 0;
 }
