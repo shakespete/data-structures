fd6c006bf1da3d5f99497b85151f13eae3ab6891
shakespete <petersyjuco@gmail.com>
2021-02-10 08:49:05 +1100

    Hash Map (string)

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -59,24 +59,103 @@ void DLL::remove(Node* v) {
 void DLL::removeFront() { if (!empty()) remove(head->next); }
 void DLL::removeBack() { if (!empty()) remove(tail->prev); }
 
+bool is_equal(const char* a, const char* b) {
+    while (*a == *b) {
+        if (*a == '\0') return true;
+        ++a;
+        ++b;
+    }
+    return false;
+}
+
+class HashMap {
+    enum { DEF_CAP = 100 };
+public:
+    HashMap(int cap = DEF_CAP);
+    int hash(const char* e);
+    void insert(const char* e);
+    void remove(const char* e);
+    Node* retrieve(const char* e);
+private:
+    DLL* HM;
+    int capacity;
+};
+
+HashMap::HashMap(int cap) : HM(new DLL[cap]), capacity(cap) { }
+int HashMap::hash(const char* e) {
+    int hash = 31;
+    int c;
+    while (c = *e++) {
+        hash = (((hash << 5) + hash) + c) % capacity;
+    }
+    return hash % capacity;
+}
+void HashMap::insert(const char* e) {
+    int hashVal = hash(e);
+    HM[hashVal].addBack(e);
+}
+void HashMap::remove(const char* e) {
+    int hashVal = hash(e);
+    if (HM[hashVal].empty()) return;
+    
+    Node* node = retrieve(e);
+    if (node != NULL) HM[hashVal].remove(node);
+    return;
+}
+Node* HashMap::retrieve(const char* e) {
+    Node* u = new Node;
+    u->val = "-";
+    int hashVal = hash(e);
+    if (HM[hashVal].empty()) return u;
+    
+    Node* node = HM[hashVal].front();
+    while (node->val != NULL) {
+        if(is_equal(e, node->val)) return node;
+        node = node->next;
+    }
+    return u;
+}
 
 int main() {
-    DLL* DList = new DLL();
-    DList->addBack("A");
-    DList->addBack("stick,");
-    DList->addBack("a");
-    DList->addBack("stone,");
-    DList->addBack("it's");
-    DList->addBack("the");
-    DList->addBack("end");
-    DList->addBack("of");
-    DList->addBack("the");
-    DList->addBack("road");
+    HashMap* hm = new HashMap();
+    hm->insert("end");
+    hm->insert("stick,");
+    hm->insert("a");
+    hm->insert("stone,");
+    hm->insert("road");
+    hm->insert("it's");
+    hm->insert("the");
+    hm->insert("A");
+    hm->insert("of");
+    hm->insert("the");
     
-    while (!DList->empty()) {
-        printf("%s ", DList->front()->val);
-        DList->removeFront();
-    }
+    
+    printf("%s ", hm->retrieve("A")->val);
+    printf("%s ", hm->retrieve("stick,")->val);
+    printf("%s ", hm->retrieve("a")->val);
+    printf("%s ", hm->retrieve("stone,")->val);
+    printf("%s ", hm->retrieve("it's")->val);
+    printf("%s ", hm->retrieve("the")->val);
+    printf("%s ", hm->retrieve("end")->val);
+    printf("%s ", hm->retrieve("of")->val);
+    printf("%s ", hm->retrieve("the")->val);
+    printf("%s\n", hm->retrieve("road")->val);
+    
+    hm->remove("A");
+    hm->remove("stone,");
+    hm->remove("end");
+    hm->remove("road");
+    printf("%s ", hm->retrieve("A")->val);
+    printf("%s ", hm->retrieve("stick,")->val);
+    printf("%s ", hm->retrieve("a")->val);
+    printf("%s ", hm->retrieve("stone,")->val);
+    printf("%s ", hm->retrieve("it's")->val);
+    printf("%s ", hm->retrieve("the")->val);
+    printf("%s ", hm->retrieve("end")->val);
+    printf("%s ", hm->retrieve("of")->val);
+    printf("%s ", hm->retrieve("the")->val);
+    printf("%s ", hm->retrieve("road")->val);
+    
     printf("\nFIN\n");
     return 0;
 }
