e267c4a3fb3ced2965ec3ba0143586f2673447d8
shakespete <petersyjuco@gmail.com>
2021-02-16 09:04:17 +1100

    Daily Kata: Hash Map

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -59,22 +59,83 @@ void DLL::remove(Node* v) {
 void DLL::removeFront() { remove(head->next); }
 void DLL::removeBack() { remove(tail->prev); }
 
+bool is_equal(const char* a, const char* b) {
+    while (*a == *b) {
+        if (*a == '\0') return true;
+        ++a;
+        ++b;
+    }
+    return false;
+}
+
+class HashMap {
+    enum { DEF_CAP = 100 };
+public:
+    HashMap(int cap = DEF_CAP);
+    int hash(const char* e);
+    void insert(const char* e);
+    void remove(const char* e);
+    Node* retrieve(const char* e);
+private:
+    DLL* HM;
+    int capacity;
+};
+
+HashMap::HashMap(int cap) : HM(new DLL[cap]), capacity(cap) {}
+int HashMap::hash(const char* e) {
+    int hash = 31;
+    int c;
+    while (c = *e++) {
+        hash = (((hash << 5) + hash) + c) % capacity;
+    }
+    return hash % capacity;
+}
+void HashMap::insert(const char* e) {
+    int hashVal = hash(e);
+    HM[hashVal].addBack(e);
+}
+void HashMap::remove(const char* e) {
+    int hashVal = hash(e);
+    if (HM[hashVal].empty()) return;
+    
+    Node* node = retrieve(e);
+    if (node != NULL) HM[hashVal].remove(node);
+    return;
+}
+Node* HashMap::retrieve(const char* e) {
+    int hashVal = hash(e);
+    if (HM[hashVal].empty()) return NULL;
+    
+    Node* node = HM[hashVal].front();
+    while (node->val != NULL) {
+        if (is_equal(e, node->val)) return node;
+        node = node->next;
+    }
+    return NULL;
+}
 
 int main() {
-    DLL* dList = new DLL();
-    dList->addBack("It's");
-    dList->addBack("the");
-    dList->addBack("rest");
-    dList->addBack("of a stump,");
-    dList->addBack("it's");
-    dList->addBack("a little");
-    dList->addBack("alone");
     
+    HashMap* HM = new HashMap();
+    HM->insert("It's");
+    HM->insert("a sliver");
+    HM->insert("of glass,");
+    HM->insert("it");
+    HM->insert("is life,");
+    HM->insert("it's");
+    HM->insert("the sun");
     
-    while (!dList->empty()) {
-        printf("%s ", dList->front()->val);
-        dList->removeFront();
-    }
+    printf("%s ", HM->retrieve("It's")->val);
+    printf("%s ", HM->retrieve("a sliver")->val);
+    printf("%s ", HM->retrieve("of glass,")->val);
+    printf("%s ", HM->retrieve("it")->val);
+    printf("%s ", HM->retrieve("is life,")->val);
+    printf("%s ", HM->retrieve("it's")->val);
+    printf("%s ", HM->retrieve("the sun")->val);
+    
+    HM->remove("it");
+    if (HM->retrieve("it") == NULL) printf("\nNULL");
+    
     printf("\nFIN\n");
     return 0;
 }
