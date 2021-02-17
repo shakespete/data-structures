d47d403d2784ad1032379d76e756ecf6f7120cf6
shakespete <petersyjuco@gmail.com>
2021-02-18 09:03:52 +1100

    Daily Kata: Circularly Linked List

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,58 +1,67 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-class DStack {
-    enum { DEF_CAP = 2 };
-public:
-    DStack(int cap = DEF_CAP);
-    ~DStack();
-    int size() const;
-    bool empty() const;
-    int top() const;
-    void push(const int e);
-    int pop();
-private:
-    int* S;
-    int t;
-    int capacity;
+struct Node {
+    const char* val;
+    Node* next;
 };
 
-DStack::DStack(int cap) : S(new int[cap]), t(-1), capacity(cap) { }
-DStack::~DStack() { while (!empty()) pop(); }
-int DStack::size() const { return t + 1; }
-bool DStack::empty() const { return size() == 0; }
-int DStack::top() const { return empty() ? -1 : S[t]; }
-void DStack::push(const int e) {
-    if (size() == capacity) {
-        int* B = new int[capacity * 2];
-        for (int i = 0; i < capacity; ++i) B[i] = S[i];
-        S = B;
-        capacity *= 2;
-    }
-    S[++t] = e;
+class CLL {
+public:
+    CLL();
+    ~CLL();
+    bool empty() const;
+    Node* front() const;
+    Node* back() const;
+    void advance();
+    void add(const char* e);
+    void remove();
+private:
+    Node* cursor;
+};
+
+CLL::CLL() : cursor(NULL) { }
+CLL::~CLL() { while (!empty()) remove(); }
+bool CLL::empty() const { return cursor == NULL; }
+Node* CLL::front() const { return empty() ? NULL : cursor->next;}
+Node* CLL::back() const { return empty() ? NULL : cursor; }
+void CLL::advance() {
+    if (!empty()) cursor = cursor->next;
 }
-int DStack::pop() {
-    if (!empty()) { return S[t--]; }
-    return -1;
+void CLL::add(const char* e) {
+    Node* v = new Node;
+    v->val = e;
+    if (empty()) {
+        v->next = v;
+        cursor = v;
+    } else {
+        v->next = cursor->next;
+        cursor->next = v;
+    }
+    
+}
+void CLL::remove() {
+    if (!empty()) {
+        Node* old = cursor->next;
+        if (old == cursor) cursor = NULL;
+        else cursor->next = old->next;
+        delete old;
+    }
+    
 }
 
 int main () {
-    DStack* st = new DStack();
-    st->push(9);
-    st->push(8);
-    st->push(7);
-    st->push(6);
-    st->push(5);
-    st->push(4);
-    st->push(3);
-    st->push(2);
-    st->push(1);
+    CLL* CL = new CLL();
+    CL->add("Hello");
+    CL->add("darkness");
+    CL->add("my");
+    CL->add("old");
+    CL->add("friend");
     
-    while (!st->empty()) {
-        printf("T: %d -> ", st->top());
-        printf("%d\n", st->pop());
+    while (!CL->empty()) {
+        printf("%s ", CL->front()->val);
+        CL->remove();
     }
     printf("\nFIN\n");
-    
     return 0;
 }
