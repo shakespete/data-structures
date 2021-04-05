47b2841390cc75637d258edd7bc5c6162ecdf7da
shakespete <petersyjuco@gmail.com>
2021-04-06 09:37:29 +1000

    Daily Kata: Linked List

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,54 +1,54 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-class DStack {
-    enum { DEF_CAP = 3 };
-public:
-    DStack(int cap = DEF_CAP);
-    ~DStack();
-    int size() const;
-    bool empty() const;
-    int top() const;
-    void push(int e);
-    int pop();
-private:
-    int* S;
-    int t;
-    int capacity;
+struct Node {
+    const char* val;
+    Node* next;
 };
 
-DStack::DStack(int cap) : S(new int[cap]), t(-1), capacity(cap) { }
-DStack::~DStack() { while (!empty()) pop(); }
-int DStack::size() const { return t + 1; }
-bool DStack::empty() const { return size() == 0;}
-int DStack::top() const { return empty() ? -1 : S[t]; }
-void DStack::push(int e) {
-    if (size() == capacity) {
-        int* B = new int[capacity * 2];
-        for (int i = 0; i < capacity; ++i) B[i] = S[i];
-        S = B;
-        capacity *= 2;
-    }
-    S[++t] = e;
+class LL {
+public:
+    LL();
+    ~LL();
+    bool empty() const;
+    Node* front() const;
+    void add(const char* e);
+    void remove();
+private:
+    Node* cursor;
+};
+
+LL::LL() : cursor(NULL) { };
+LL::~LL() { while (!empty()) remove(); }
+bool LL::empty() const { return cursor == NULL; }
+Node* LL::front() const { return empty() ? NULL : cursor; }
+void LL::add(const char* e) {
+    Node* v = new Node;
+    v->val = e;
+    v->next = cursor;
+    cursor = v;
 }
-int DStack::pop() { return empty() ? -1 : S[t--]; }
+void LL::remove() {
+    if (!empty()) {
+        Node* old = cursor;
+        cursor = cursor->next;
+        delete old;
+    }
+}
 
 int main() {
-    DStack* st = new DStack();
+    LL* list = new LL();
+    list->add("the bone");
+    list->add("and");
+    list->add("the flesh");
+    list->add("the ground,");
+    list->add("The foot,");
     
-    st->push(9);
-    st->push(8);
-    st->push(7);
-    st->push(6);
-    st->push(5);
-    st->push(4);
-    st->push(3);
-    st->push(2);
-    st->push(1);
-    
-    while (!st->empty()) {
-        printf("%d ", st->pop());
+    while (!list->empty()) {
+        printf("%s ", list->front()->val);
+        list->remove();
     }
     printf("\nFIN\n");
+    
     return 0;
 }
