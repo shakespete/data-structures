166eefa5894cc1ea0609326a89802e1f3255c9c3
shakespete <petersyjuco@gmail.com>
2021-05-06 10:58:08 +1000

    Daily Kata: Hash Map

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -59,21 +59,84 @@ void DLL::remove(Node* v) {
 void DLL::removeFront() { if (!empty()) remove(head->next); }
 void DLL::removeBack() { if (!empty()) remove(tail->prev); }
 
-int main() {
-    DLL* dlist = new DLL();
-    dlist->addBack("A mile,");
-    dlist->addBack("a must,");
-    dlist->addBack("a thrust,");
-    dlist->addBack("a bump,");
-    dlist->addBack("It's a girl,");
-    dlist->addBack("it's a rhyme,");
-    dlist->addBack("it's a cold,");
-    dlist->addBack("it's the mumps.");
-
-    while(!dlist->empty()) {
-        printf("%s ", dlist->front()->val);
-        dlist->removeFront();
+bool is_equal(const char* a, const char* b) {
+    while (*a == *b) {
+        if (*a == '\0') return true;
+        ++a;
+        ++b;
     }
+    return false;
+}
+
+class HashMap {
+    enum { DEF_CAP = 100 };
+public:
+    HashMap(int cap = DEF_CAP);
+    int hash(const char* e) const;
+    void insert(const char* e) const;
+    int remove(const char* e) const;
+    Node* retrieve(const char* e) const;
+private:
+    DLL* HM;
+    int capacity;
+};
+
+HashMap::HashMap(int cap) : HM(new DLL[cap]), capacity(cap) { } ;
+int HashMap::hash(const char* e) const {
+    int hash = 31;
+    int c;
+    while (c = *e++) {
+        hash = (((hash << 5) + hash) + c) % capacity;
+    }
+    return hash % capacity;
+}
+void HashMap::insert(const char* e) const {
+    int hashVal = hash(e);
+    HM[hashVal].addBack(e);
+}
+int HashMap::remove(const char* e) const {
+    int hashVal = hash(e);
+    if (HM[hashVal].empty()) return 0;
+    
+    Node* node = retrieve(e);
+    if (node != NULL) {
+        HM[hashVal].remove(node);
+        return 1;
+    };
+    return 0;
+}
+Node* HashMap::retrieve(const char* e) const {
+    int hashVal = hash(e);
+    if (HM[hashVal].empty()) return NULL;
+    
+    Node* node = HM[hashVal].front();
+    while (node->val != NULL) {
+        if (is_equal(e, node->val)) return node;
+        node = node->next;
+    }
+    return NULL;
+}
+
+int main() {
+    HashMap* hm = new HashMap();
+    
+    hm->insert("A mile,");
+    hm->insert("a must,");
+    hm->insert("a thrust,");
+    hm->insert("a bump,");
+    hm->insert("It's a girl,");
+    hm->insert("it's a rhyme,");
+    hm->insert("it's a cold,");
+    hm->insert("it's the mumps.");
+
+    printf("%d\n", hm->remove("it's the mumps."));
+    printf("%s ", hm->retrieve("A mile,")->val);
+    printf("%s ", hm->retrieve("a must,")->val);
+    printf("%s ", hm->retrieve("a thrust,")->val);
+    printf("%s ", hm->retrieve("a bump,")->val);
+    printf("%s ", hm->retrieve("It's a girl,")->val);
+    printf("%s ", hm->retrieve("it's a rhyme,")->val);
+    printf("%s ", hm->retrieve("it's a cold,")->val);
     printf("\nFIN\n");
     return 0;
 }
