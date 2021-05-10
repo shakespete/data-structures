882d95df87f8834990b396262d3bdd822f88ce94
shakespete <petersyjuco@gmail.com>
2021-05-10 10:46:07 +1000

    Daily Kata: Circularly Linked List

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -4,139 +4,62 @@
 struct Node {
     const char* val;
     Node* next;
-    Node* prev;
 };
 
-class DLL {
+class CLL {
 public:
-    DLL();
-    ~DLL();
+    CLL();
+    ~CLL();
     bool empty() const;
     Node* front() const;
     Node* back() const;
-    void add(Node* v, const char* e);
-    void addFront(const char* e);
-    void addBack(const char* e);
-    void remove(Node* v);
-    void removeFront();
-    void removeBack();
+    void add(const char* e);
+    void advance();
+    void remove();
 private:
-    Node* head;
-    Node* tail;
+    Node* cursor;
 };
 
-DLL::DLL() {
-    head = new Node;
-    tail = new Node;
-    head->next = tail;
-    tail->prev = head;
-    head->val = NULL;
-    tail->val = NULL;
+CLL::CLL() : cursor(NULL) { }
+CLL::~CLL() { while (!empty()) remove(); }
+bool CLL::empty() const { return cursor == NULL;}
+Node* CLL::front() const { return empty() ? NULL : cursor->next; }
+Node* CLL::back() const { return empty() ? NULL : cursor; }
+void CLL::add(const char* e) {
+    Node* v = new Node;
+    v->val = e;
+    if (empty()) {
+        v->next = v;
+        cursor = v;
+    } else {
+        v->next = cursor->next;
+        cursor->next = v;
+    }
 }
-DLL::~DLL() { while (!empty()) removeFront(); }
-bool DLL::empty() const { return head->next == tail; }
-Node* DLL::front() const { return empty() ? NULL : head->next; }
-Node* DLL::back() const { return empty() ? NULL : tail->prev; }
-void DLL::add(Node* v, const char* e) {
-    Node* u = new Node;
-    u->val = e;
-    u->prev = v->prev;
-    u->next = v;
-    v->prev->next = u;
-    v->prev = u;
-}
-void DLL::addFront(const char* e) { add(head->next, e); }
-void DLL::addBack(const char* e) { add(tail, e); }
-void DLL::remove(Node* v) {
+void CLL::advance() { if (!empty()) cursor = cursor->next; }
+void CLL::remove() {
     if (!empty()) {
-        Node* u = v->prev;
-        Node* w = v->next;
-        u->next = w;
-        w->prev = u;
-        delete v;
+        Node* old = cursor->next;
+        if (old == cursor)  cursor = NULL;
+        else cursor->next = old->next;
+        delete old;
     }
-}
-void DLL::removeFront() { if (!empty()) remove(head->next); }
-void DLL::removeBack() { if (!empty()) remove(tail->prev); }
-
-bool is_equal(const char* a, const char* b) {
-    while (*a == *b) {
-        if (*a == '\0') return true;
-        ++a;
-        ++b;
-    }
-    return false;
-}
-
-class HashMap {
-    enum { DEF_CAP = 100 };
-public:
-    HashMap(int cap = DEF_CAP);
-    int hash(const char* e) const;
-    void insert(const char* e) const;
-    int remove(const char* e) const;
-    Node* retrieve(const char* e) const;
-private:
-    DLL* HM;
-    int capacity;
-};
-
-HashMap::HashMap(int cap) : HM(new DLL[cap]), capacity(cap) { } ;
-int HashMap::hash(const char* e) const {
-    int hash = 31;
-    int c;
-    while (c = *e++) {
-        hash = (((hash << 5) + hash) + c) % capacity;
-    }
-    return hash % capacity;
-}
-void HashMap::insert(const char* e) const {
-    int hashVal = hash(e);
-    HM[hashVal].addBack(e);
-}
-int HashMap::remove(const char* e) const {
-    int hashVal = hash(e);
-    if (HM[hashVal].empty()) return 0;
-    
-    Node* node = retrieve(e);
-    if (node != NULL) {
-        HM[hashVal].remove(node);
-        return 1;
-    };
-    return 0;
-}
-Node* HashMap::retrieve(const char* e) const {
-    int hashVal = hash(e);
-    if (HM[hashVal].empty()) return NULL;
-    
-    Node* node = HM[hashVal].front();
-    while (node->val != NULL) {
-        if (is_equal(e, node->val)) return node;
-        node = node->next;
-    }
-    return NULL;
 }
 
 int main() {
-    HashMap* hm = new HashMap();
+    CLL* clist = new CLL();
     
-    hm->insert("A mile,");
-    hm->insert("a must,");
-    hm->insert("a thrust,");
-    hm->insert("a bump,");
-    hm->insert("It's a girl,");
-    hm->insert("it's a rhyme,");
-    hm->insert("it's a cold,");
-    hm->insert("it's the mumps.");
-
-    printf("%d\n", hm->remove("it's the mumps."));
-    printf("%s ", hm->retrieve("A mile,")->val);
-    printf("%s ", hm->retrieve("a must,")->val);
-    printf("%s ", hm->retrieve("a thrust,")->val);
-    printf("%s ", hm->retrieve("a bump,")->val);
-    printf("%s ", hm->retrieve("It's a girl,")->val);
-    printf("%s ", hm->retrieve("it's a rhyme,")->val);
-    printf("%s ", hm->retrieve("it's a cold,")->val);
+    clist->add("a bite,");
+    clist->add("a bee,");
+    clist->add("a grain,");
+    clist->add("A point,");
+    
+    while (!clist->empty()) {
+        printf("%s ", clist->front()->val);
+        clist->remove();
+    }
+    
     printf("\nFIN\n");
+    
     return 0;
 }
