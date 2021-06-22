cf20f05685f408d00942314183191b7df9e54611
shakespete <petersyjuco@gmail.com>
2021-06-22 10:33:16 +1000

    Daily Kata: Hash Map

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -59,18 +59,87 @@ void DLL::remove(Node* v) {
 void DLL::removeFront() { if (!empty()) remove(head->next); }
 void DLL::removeBack() { if (!empty()) remove(tail->prev); }
 
-int main() {
-    DLL* dlist = new DLL();
-    
-    dlist->addBack("A night,");
-    dlist->addBack("a death,");
-    dlist->addBack("the end");
-    dlist->addBack("of the run");
-    
-    while (!dlist->empty()) {
-        printf("%s ", dlist->front()->val);
-        dlist->removeFront();
+bool is_equal(const char* a, const char* b) {
+    while (*a == *b) {
+        if (*a == '\0') return true;
+        ++a;
+        ++b;
     }
+    return false;
+}
+
+class HashMap {
+    enum { DEF_CAP = 100 };
+public:
+    HashMap(int cap = DEF_CAP);
+    int hash(const char* e);
+    void insert(const char* e);
+    int remove(const char* e);
+    Node*  retrieve(const char* e);
+private:
+    DLL* HM;
+    int capacity;
+};
+
+HashMap::HashMap(int cap) : HM(new DLL[cap]), capacity(cap) { };
+int HashMap::hash(const char* e) {
+    int hash = 31;
+    int c;
+    while ((c = *e++)) {
+        hash = (((hash << 5) + hash) + c) % capacity;
+    }
+    return hash % capacity;
+}
+void HashMap::insert(const char* e) {
+    int hashVal = hash(e);
+    HM[hashVal].addBack(e);
+}
+int HashMap::remove(const char* e) {
+    int hashVal = hash(e);
+    if (HM[hashVal].empty()) return 0;
+    
+    Node* node = retrieve(e);
+    if (node != NULL) {
+        HM[hashVal].remove(node);
+        return 1;
+    }
+    return 0;
+}
+Node* HashMap::retrieve(const char* e) {
+    int hashVal = hash(e);
+    if (HM[hashVal].empty()) return NULL;
+    
+    Node* node = HM[hashVal].front();
+    while (node != NULL) {
+        if (is_equal(e, node->val)) return node;
+        node = node->next;
+    }
+    return NULL;
+}
+
+
+int main() {
+    HashMap* hm = new HashMap();
+    hm->insert("A");
+    hm->insert("night");
+    hm->insert("a");
+    hm->insert("death");
+    hm->insert("the");
+    hm->insert("end");
+    hm->insert("of");
+    hm->insert("the");
+    hm->insert("run");
+    
+    printf("%s ", hm->retrieve("A")->val);
+    printf("%s ", hm->retrieve("night")->val);
+    printf("%s ", hm->retrieve("a")->val);
+    printf("%s ", hm->retrieve("death")->val);
+    printf("%s ", hm->retrieve("the")->val);
+    printf("%s ", hm->retrieve("end")->val);
+    printf("%d ", hm->remove("of"));
+    printf("%d ", hm->remove("the"));
+    printf("%d ", hm->remove("run"));
+    
     printf("\nFIN\n");
     return 0;
 }
