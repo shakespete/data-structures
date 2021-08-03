2118c1b1e9267b7af549e7e338e861671e506dc3
shakespete <petersyjuco@gmail.com>
2021-08-03 20:01:17 +1000

    Daily Kata: Linked List

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -6,87 +6,48 @@ struct Node {
     Node* next;
 };
 
-class CLL {
+class LL {
 public:
-    CLL();
-    ~CLL();
+    LL();
+    ~LL();
     bool empty() const;
     Node* front() const;
-    Node* back() const;
     void add(const char* e);
-    void advance();
     void remove();
 private:
-    Node* cursor;
+    Node* head;
 };
 
-CLL::CLL() : cursor(NULL) { }
-CLL::~CLL() { while (!empty()) remove(); }
-bool CLL::empty() const { return cursor == NULL; }
-Node* CLL::front() const { return empty() ? NULL : cursor->next; }
-Node* CLL::back() const { return empty() ? NULL : cursor; }
-void CLL::add(const char* e) {
-    Node* v = new Node;
-    v->val = e;
-    if (empty()) {
-        v->next = v;
-        cursor = v;
-    } else {
-        v->next = cursor->next;
-        cursor->next = v;
-    }
-}
-void CLL::advance() { if (!empty()) cursor = cursor->next; }
-void CLL::remove() {
-    Node* old = cursor->next;
-    if (cursor == old) cursor = NULL;
-    else cursor->next = old->next;
-    delete old;
-}
 
-class Queue {
-public:
-    Queue();
-    ~Queue();
-    int size() const;
-    bool empty() const;
-    Node* front() const;
-    void enq(const char* e);
-    void deq();
-private:
-    CLL CL;
-    int n;
-};
-
-Queue::Queue() : CL(), n(0) {}
-Queue::~Queue() { while (!empty()) deq(); }
-int Queue::size() const { return n; }
-bool Queue::empty() const { return n == 0; }
-Node* Queue::front() const { return empty() ? NULL : CL.front(); }
-void Queue::enq(const char* e) {
-    CL.add(e);
-    CL.advance();
-    ++n;
+LL::LL() : head(NULL) { }
+LL::~LL() { while (!empty()) remove(); }
+bool LL::empty() const { return head == NULL;}
+Node* LL::front() const { return empty() ? NULL : head; }
+void LL::add(const char* e) {
+    Node* node = new Node;
+    node->val = e;
+    node->next = head;
+    head = node;
 }
-void Queue::deq() {
+void LL::remove() {
     if (!empty()) {
-        CL.remove();
-        --n;
+        Node* old = head;
+        head = old->next;
+        delete old;
     }
 }
 
 int main() {
+    LL* l = new LL();
     
-    Queue* q = new Queue();
-       
-    q->enq("It's the rest");
-    q->enq("of a stump,");
-    q->enq("it's a little");
-    q->enq("alone,");
+    l->add("of the road,");
+    l->add("it's the end");
+    l->add("a stone,");
+    l->add("A stick,");
     
-    while (!q->empty()) {
-        printf("%s ", q->front()->val);
-        q->deq();
+    while (!l->empty()) {
+        printf("%s ", l->front()->val);
+        l->remove();
     }
     printf("\nFIN\n");
     
