8792226d58057db61069d1fb67e62eeb8e719827
shakespete <petersyjuco@gmail.com>
2021-09-16 18:00:17 +1000

    Stack

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,63 +1,48 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-struct Node {
-    const char* val;
-    Node* next;
-};
-
-class CLL {
+class Stack {
+    enum { DEF_CAP = 10 };
 public:
-    CLL();
-    ~CLL();
-    bool empty() const;
-    Node* front() const;
-    void add(const char* e);
-    void advance();
-    void remove();
+    Stack(int cap = DEF_CAP);
+    ~Stack();
+    bool empty();
+    void push(int e);
+    int pop();
 private:
-    Node* cursor;
+    int* S;
+    int top;
+    int capacity;
 };
 
-CLL::CLL() : cursor(NULL) {};
-CLL::~CLL() { while (!empty()) remove(); }
-bool CLL::empty() const { return cursor == NULL; }
-Node* CLL::front() const { return empty() ? NULL : cursor->next; }
-void CLL::add(const char* e) {
-    Node* v = new Node;
-    v->val = e;
-    if (empty()) {
-        v->next = v;
-        cursor = v;
-    } else {
-        v->next = cursor->next;
-        cursor->next = v;
+Stack::Stack(int cap) : S(new int[cap]), top(0), capacity(cap) { };
+Stack::~Stack() { while (!empty()) pop(); }
+bool Stack::empty() { return top == 0; }
+void Stack::push(int e) {
+    if (top != capacity) {
+        S[++top] = e;
     }
 }
-void CLL::advance() { if (!empty()) cursor = cursor->next; }
-void CLL::remove() {
+int Stack::pop() {
     if (!empty()) {
-        Node* old = cursor->next;
-        if (old == cursor) cursor = NULL;
-        else cursor->next = old->next;
-        delete old;
+        return S[top--];
     }
+    return -1;
 }
-
 
 int main() {
-    CLL* clist = new CLL();
-    clist->add("take ya");
-    clist->add("I want to");
-    clist->add("oh");
-    clist->add("Jamaica,");
-    clist->add("Aruba,");
-    
-    while (!clist->empty()) {
-        printf("%s ", clist->front()->val);
-        clist->remove();
+    Stack* st = new Stack();
+    st->push(1);
+    st->push(2);
+    st->push(3);
+    st->push(4);
+    st->push(5);
+    st->push(6);
+    st->push(7);
+    st->push(8);
+    while (!st->empty()) {
+        printf("%d\n", st->pop());
     }
-    
-    printf("\nFIN\n");
+    printf("FIN\n");
     return 0;
 }
