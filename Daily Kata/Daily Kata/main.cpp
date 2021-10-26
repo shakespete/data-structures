cf831bd13398d998fcc45a7be4495e44076ed2a4
shakespete <petersyjuco@gmail.com>
2021-10-26 23:18:09 +1100

    Circular Linked List

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -3,74 +3,62 @@
 
 struct Node {
     const char* val;
-    Node* prev;
     Node* next;
 };
 
-class DLL {
+class CLL {
 public:
-    DLL();
-    ~DLL();
+    CLL();
+    ~CLL();
     bool empty() const;
     Node* front() const;
     Node* back() const;
-    void add(Node* v, const char* e);
-    void addFront(const char* e);
-    void addBack(const char* e);
-    void remove(Node* v);
-    void removeFront();
-    void removeBack();
+    void advance();
+    void add(const char* e);
+    void remove();
 private:
-    Node* head;
-    Node* tail;
+    Node* cursor;
 };
 
-DLL::DLL() {
-    head = new Node();
-    tail = new Node();
-    head->val = nullptr;
-    tail->val = nullptr;
-    head->next = tail;
-    tail->prev = head;
-};
-DLL::~DLL() { while (!empty()) removeFront(); }
-bool DLL::empty() const { return head->next == tail;}
-Node* DLL::front() const { return empty() ? nullptr : head->next; }
-Node* DLL::back() const { return empty() ? nullptr : tail->prev; }
-void DLL::add(Node* v, const char* e) {
+CLL::CLL() : cursor(nullptr) { }
+CLL::~CLL() { while (!empty()) remove(); }
+bool CLL::empty() const { return cursor == nullptr; }
+Node* CLL::front() const { return empty() ? nullptr : cursor->next; }
+Node* CLL::back() const { return empty() ? nullptr : cursor; }
+void CLL::advance() { if (empty()) cursor = cursor->next; }
+void CLL::add(const char* e) {
     Node* u = new Node();
     u->val = e;
-    u->next = v;
-    u->prev = v->prev;
-    v->prev->next = u;
-    v->prev = u;
-};
-void DLL::addFront(const char* e) { add(head->next, e); }
-void DLL::addBack(const char* e) { add(tail, e); }
-void DLL::remove(Node* v) {
-    if (!empty()) {
-        Node* u = v->prev;
-        Node* w = v->next;
-        u->next = w;
-        w->prev = u;
-        delete v;
+    if (cursor == NULL) {
+        u->next = u;
+        cursor = u;
+    } else {
+        u->next = cursor->next;
+        cursor->next = u;
     }
 }
-void DLL::removeFront() { if (!empty()) remove(head->next); }
-void DLL::removeBack() { if (!empty()) remove(tail->prev); }
+void CLL::remove() {
+    Node* old = cursor->next;
+    if (old == cursor) cursor = nullptr;
+    else cursor->next = old->next;
+    delete old;
+}
 
 int main() {
-    DLL* dlist = new DLL();
-    dlist->addBack("darkness");
-    dlist->addFront("Hello");
-    dlist->addBack("my");
-    dlist->addBack("old");
-    dlist->addBack("friend");
+    CLL* clist = new CLL();
+    clist->add("ya");
+    clist->add("take");
+    clist->add("wanna");
+    clist->add("I");
+    clist->add("oooh");
+    clist->add("Jamaica");
+    clist->add("Aruba");
     
-    while(!dlist->empty()) {
-        printf("%s ", dlist->front()->val);
-        dlist->removeFront();
+    while (!clist->empty()) {
+        printf("%s ", clist->front()->val);
+        clist->remove();
     }
+    
     printf("\nFIN\n");
     return 0;
 }
