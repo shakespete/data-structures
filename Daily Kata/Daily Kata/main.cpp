27ea61ec4ecd8220bd828b1227a03807d0a7182c
shakespete <petersyjuco@gmail.com>
2021-10-31 10:39:31 +1100

    Stack

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,64 +1,57 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-struct Node {
-    const char* val;
-    Node* next;
-};
-
-class CLL {
+class Stack {
+    enum { DEF_CAP = 2 };
 public:
-    CLL();
-    ~CLL();
+    Stack(int cap = DEF_CAP);
+    ~Stack();
+    int size() const;
     bool empty() const;
-    Node* front() const;
-    Node* back() const;
-    void advance();
-    void add(const char* e);
-    void remove();
+    int top() const;
+    void push(int e);
+    void pop();
 private:
-    Node* cursor;
+    int *S;
+    int t;
+    int capacity;
 };
 
-CLL::CLL() : cursor(nullptr) { }
-CLL::~CLL() { while (!empty()) remove(); }
-bool CLL::empty() const { return cursor == nullptr; }
-Node* CLL::front() const { return empty() ? nullptr : cursor->next; }
-Node* CLL::back() const { return empty() ? nullptr : cursor; }
-void CLL::advance() { if (empty()) cursor = cursor->next; }
-void CLL::add(const char* e) {
-    Node* u = new Node();
-    u->val = e;
-    if (cursor == NULL) {
-        u->next = u;
-        cursor = u;
-    } else {
-        u->next = cursor->next;
-        cursor->next = u;
+Stack::Stack(int cap) : S(new int[cap]), t(-1), capacity(cap) { }
+Stack::~Stack() { while (!empty()) pop(); }
+int Stack::size() const { return t + 1; }
+bool Stack::empty() const { return size() == 0; }
+int Stack::top() const { return empty() ? -1 : S[t]; }
+void Stack::push(int e) {
+    if (size() == capacity) {
+        int *T = new int[capacity * 2];
+        for (int i = 0; i < capacity; ++i) T[i] = S[i];
+        S = T;
+        capacity *= 2;
     }
+    S[++t] = e;
 }
-void CLL::remove() {
-    Node* old = cursor->next;
-    if (old == cursor) cursor = nullptr;
-    else cursor->next = old->next;
-    delete old;
-}
+void Stack::pop() { if (!empty()) t--; }
 
 int main() {
-    CLL* clist = new CLL();
-    clist->add("ya");
-    clist->add("take");
-    clist->add("wanna");
-    clist->add("I");
-    clist->add("oooh");
-    clist->add("Jamaica");
-    clist->add("Aruba");
+    Stack* st = new Stack();
+    st->push(1);
+    st->push(2);
+    st->push(3);
+    st->push(4);
+    st->push(5);
+    st->push(6);
+    st->push(7);
+    st->push(8);
+    st->push(9);
+    st->push(10);
     
-    while (!clist->empty()) {
-        printf("%s ", clist->front()->val);
-        clist->remove();
+    printf("SIZE: %d\n", st->size());
+    while (!st->empty()) {
+        printf("%d ", st->top());
+        st->pop();
     }
-    
+    printf("\nSIZE: %d", st->size());
     printf("\nFIN\n");
     return 0;
 }
