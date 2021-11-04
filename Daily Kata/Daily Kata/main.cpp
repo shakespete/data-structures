15bf13887f25fb047f26e5a945d1bf02e9e06456
shakespete <petersyjuco@gmail.com>
2021-11-04 11:35:44 +1100

    Doubly Linked List

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -4,113 +4,77 @@
 
 using namespace std;
 
-class Node {
-public:
-    int value;
-    Node *left;
-    Node *right;
-    Node *parent;
-    Node(int e) {
-        value = e;
-        left = nullptr;
-        right = nullptr;
-        parent = nullptr;
-    }
+struct Node {
+    const char *val;
+    Node *prev;
+    Node *next;
 };
 
-class BST {
+class DLL{
 public:
-    Node *root;
-    Node *treeSearch(Node *x, int e) const;
-    Node *treeMin(Node *x) const;
-    void insert(int e);
-    void remove(int e);
-    void transplant(Node *u, Node *v);
-    void inorderTraversal(Node* x) const;
+    DLL();
+    ~DLL();
+    bool empty() const;
+    Node *front() const;
+    Node *back() const;
+    void add(Node *v, const char *e);
+    void addFront(const char *e);
+    void addBack(const char *e);
+    void remove(Node *v);
+    void removeFront();
+    void removeBack();
+private:
+    Node *head;
+    Node *tail;
 };
 
-Node *BST::treeSearch(Node *x, int e) const {
-    while (x && x->value != e) {
-        if (e < x->value) x = x->left;
-        else x = x->right;
-    }
-    return x;
+DLL::DLL() {
+    head = new Node();
+    tail = new Node();
+    head->val = nullptr;
+    tail->val = nullptr;
+    head->next = tail;
+    tail->prev = head;
 }
-Node *BST::treeMin(Node *x) const {
-    while (x->left) x = x->left;
-    return x;
+DLL::~DLL() { while (!empty()) removeFront(); }
+bool DLL::empty() const { return head->next == tail; }
+Node *DLL::front() const { return empty() ? nullptr : head->next; }
+Node *DLL::back() const { return empty() ? nullptr : tail->prev; }
+void DLL::add(Node *v, const char *e) {
+    Node *u = new Node();
+    u->val = e;
+    u->next = v;
+    u->prev = v->prev;
+    v->prev->next = u;
+    v->prev = u;
 }
-void BST::insert(int e) {
-    Node *x = root;
-    Node *y = nullptr;
-    Node *z = new Node(e);
-    
-    while (x) {
-        y = x;
-        if (z->value < x->value) x = x->left;
-        else x = x->right;
-    }
-    
-    z->parent = y;
-    if (!y) root = z;
-    else if (z->value < y->value) y->left = z;
-    else y->right = z;
-}
-void BST::remove(int e) {
-    Node *z = treeSearch(root, e);
-    
-    if (!z->left) transplant(z, z->right);
-    else if (!z->right) transplant(z, z->left);
-    else {
-        Node *y = treeMin(z->right);
-        
-        if (y->parent != z) {
-            transplant(y, y->right);
-            y->right = z->right;
-            y->right->parent = y;
-        }
-        
-        transplant(z, y);
-        y->left = z->left;
-        y->left->parent = y;
+void DLL::addFront(const char *e) { add(head->next, e); }
+void DLL::addBack(const char *e) { add(tail, e); }
+void DLL::remove(Node *v) {
+    if (!empty()) {
+        Node *u = v->prev;
+        Node *w = v->next;
+        u->next = w;
+        w->prev = u;
+        delete v;
     }
 }
-void BST::transplant(Node *u, Node *v) {
-    if (!u->parent) root = v;
-    else if (u->parent->left == u) u->parent->left = v;
-    else u->parent->right = v;
-    
-    if (v) v->parent = u->parent;
-}
-void BST::inorderTraversal(Node* x) const {
-    if (x) {
-        inorderTraversal(x->left);
-        printf("%d ", x->value);
-        inorderTraversal(x->right);
-    }
-}
+void DLL::removeFront() { if (!empty()) remove(head->next); }
+void DLL::removeBack() { if (!empty()) remove(tail->prev); }
 
 int main() {
-    BST *bst = new BST();
-    bst->insert(12);
-    bst->insert(5);
-    bst->insert(18);
-    bst->insert(2);
-    bst->insert(9);
-    bst->insert(15);
-    bst->insert(19);
-    bst->insert(13);
-    bst->insert(17);
+    DLL *dlist = new DLL();
+    dlist->addBack("no");
+    dlist->addBack("strangers");
+    dlist->addBack("to");
+    dlist->addBack("love");
+    dlist->addFront("We're");
     
-    bst->inorderTraversal(bst->root);
-    printf("\nRemove 19\n");
-    bst->remove(19);
-    bst->inorderTraversal(bst->root);
-    printf("\nInsert 22\n");
-    bst->insert(22);
-    bst->remove(17);
-    printf("\nRemove 17\n");
-    bst->inorderTraversal(bst->root);
+    while (!dlist->empty()) {
+        printf("%s ", dlist->front()->val);
+        dlist->removeFront();
+    }
+    
     printf("\nFIN\n");
     return 0;
 }
