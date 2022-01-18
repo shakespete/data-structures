245038124cee77c5ff618e29bf42c1936e64f8c5
shakespete <petersyjuco@gmail.com>
2022-01-18 17:57:58 +1100

    Dynamic Stack

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,112 +1,57 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <stack>
 #include <vector>
 
 using namespace std;
 
-struct Node {
-    string val;
-    Node* prev;
-    Node* next;
-    Node(string s) {
-        val = s;
-        prev = nullptr;
-        next = nullptr;
-    }
-};
-
-class DLL {
+class DStack {
+    enum { DEF_CAP = 3 };
 public:
-    DLL();
-    ~DLL();
-    bool empty() const;
-    Node* front() const;
-    Node* back() const;
-    void add(Node* v, string s);
-    void addFront(string s);
-    void addBack(string s);
-    void remove(Node *v);
-    void removeFront();
-    void removeBack();
-private:
-    Node* head;
-    Node* tail;
-};
-
-DLL::DLL() {
-    head = new Node("*");
-    tail = new Node("*");
-    head->next = tail;
-    tail->prev = head;
-}
-DLL::~DLL() { while (!empty()) removeFront(); }
-bool DLL::empty() const { return head->next == tail; }
-Node* DLL::front() const { return empty() ? nullptr : head->next; }
-Node* DLL::back() const { return empty() ? nullptr : tail->prev; }
-void DLL::add(Node* v, string s) {
-    Node* u = new Node(s);
-    u->next = v;
-    u->prev = v->prev;
-    v->prev->next = u;
-    v->prev = u;
-}
-void DLL::addFront(string s) { add(head->next, s); }
-void DLL::addBack(string s) { add(tail, s); }
-void DLL::remove(Node *v) {
-    if (!empty()) {
-        Node* u = v->prev;
-        Node* w = v->next;
-        u->next = w;
-        w->prev = u;
-        delete v;
-    }
-}
-void DLL::removeFront() { if (!empty()) remove(head->next); }
-void DLL::removeBack() { if (!empty()) remove(tail->prev); }
-
-class Queue {
-public:
-    Queue();
-    ~Queue();
+    DStack(int cap = DEF_CAP);
+    ~DStack();
     int size() const;
     bool empty() const;
-    Node* front() const;
-    void enq(string s);
-    void deq();
+    int top() const;
+    void push(int e);
+    void pop();
 private:
-    DLL dlist;
-    int n;
+    int* S;
+    int t;
+    int capacity;
 };
 
-Queue::Queue() : dlist(), n(0) { }
-Queue::~Queue() { while (!empty()) deq(); }
-int Queue::size() const { return n; }
-bool Queue::empty() const { return n == 0; }
-Node* Queue::front() const { return empty() ? nullptr : dlist.front(); }
-void Queue::enq(string s) {
-    dlist.addBack(s);
-    ++n;
-}
-void Queue::deq() {
-    if (!empty()) {
-        dlist.removeFront();
-        --n;
+DStack::DStack(int cap) : S(new int[cap]), t(-1), capacity(cap) {}
+DStack::~DStack() { while (!empty()) pop(); }
+int DStack::size() const { return t + 1; }
+bool DStack::empty() const { return size() == 0; }
+int DStack::top() const { return empty() ? -1 : S[t]; }
+void DStack::push(int e) {
+    if (size() == capacity) {
+        int* T = new int[capacity * 2];
+        for (int i = 0; i < capacity; ++i) T[i] = S[i];
+        S = T;
+        capacity = capacity * 2;
     }
+    S[++t] = e;
 }
+void DStack::pop() { if (!empty()) --t; }
 
 int main() {
-        
-    Queue* q = new Queue();
-    q->enq("There is nothing");
-    q->enq("either good");
-    q->enq("or bad,");
-    q->enq("but thinking");
-    q->enq("makes it so.");
+    DStack* stack = new DStack();
+    stack->push(10);
+    stack->push(9);
+    stack->push(8);
+    stack->push(7);
+    stack->push(6);
+    stack->push(5);
+    stack->push(4);
+    stack->push(3);
+    stack->push(2);
+    stack->push(1);
     
-    while (!q->empty()) {
-        cout << q->front()->val << " ";
-        q->deq();
+    while (!stack->empty()) {
+        cout << stack->top() << " ";
+        stack->pop();
     }
     
     std::cout << "\nFIN\n";
