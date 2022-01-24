ca178220cf261c506f933057098c9526321b729b
shakespete <petersyjuco@gmail.com>
2022-01-24 17:42:17 +1100

    Hash Map

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -37,6 +37,7 @@ DLList::DLList() {
     head->val = "*";
     tail->val = "*";
 }
+DLList::~DLList() { while (!empty()) removeBack(); }
 bool DLList::empty() const { return head->next == tail; }
 Node* DLList::front() const { return empty() ? nullptr : head->next; }
 Node* DLList::back() const { return empty() ? nullptr : tail->prev; }
@@ -63,21 +64,66 @@ void DLList::remove(Node* v) {
 void DLList::removeFront() { if (!empty()) remove(head->next); }
 void DLList::removeBack() { if (!empty()) remove(tail->prev); }
 
-int main() {
-    DLList* dlist = new DLList();
-    dlist->addBack("We are");
-    dlist->addBack("such stuff");
-    dlist->addBack("as dreams");
-    dlist->addBack("are made on,");
-    dlist->addBack("and our");
-    dlist->addBack("little life");
-    dlist->addBack("is rounded");
-    dlist->addBack("with a sleep.");
-    
-    while (!dlist->empty()) {
-        cout << dlist->front()->val << " ";
-        dlist->removeFront();
+class HashMap {
+    enum { DEF_CAP = 100 };
+public:
+    HashMap(int cap = DEF_CAP);
+    int hash(string str);
+    void insert(string str);
+    Node* retrieve(string str);
+    void remove(string str);
+private:
+    DLList* HM;
+    int capacity;
+};
+
+HashMap::HashMap(int cap) : HM(new DLList[cap]), capacity(cap) { };
+int HashMap::hash(string str) {
+    int hash = 5381;
+    for (int i = 0; i < (int)str.size(); ++i) {
+        hash = (((hash << 5) + hash) + str[i]) % capacity;
     }
+    return hash;
+}
+void HashMap::insert(string str) {
+    int hashVal = hash(str);
+    HM[hashVal].addBack(str);
+}
+Node* HashMap::retrieve(string str) {
+    int hashVal = hash(str);
+//    cout << hashVal << " ";
+    if (HM[hashVal].empty()) return nullptr;
+    
+    Node* node = HM[hashVal].front();
+    while (node) {
+        if (str.compare(node->val) == 0) return node;
+        node = node->next;
+    }
+    return nullptr;
+}
+void HashMap::remove(string str) {
+    int hashVal = hash(str);
+    if (HM[hashVal].empty()) return;
+    
+    Node* node = retrieve(str);
+    if (node) HM[hashVal].remove(node);
+    return;
+}
+
+int main() {
+    HashMap* hm = new HashMap();
+    hm->insert("Hell");
+    hm->insert("is empty");
+    hm->insert("and all");
+    hm->insert("the devils");
+    hm->insert("are here");
+    
+    cout << hm->retrieve("Hell")->val << " ";
+    cout << hm->retrieve("is empty")->val << " ";
+    cout << hm->retrieve("and all")->val << " ";
+    cout << hm->retrieve("the devils")->val << " ";
+    cout << hm->retrieve("are here")->val << " ";
+    
     std::cout << "\nFIN\n";
     return 0;
 }
