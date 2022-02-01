1922f728dd53b979a8e746fb134ed2acba63a758
shakespete <petersyjuco@gmail.com>
2022-02-01 21:53:37 +1100

    Stack

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,67 +1,56 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
-struct TrieNode {
-    bool isEnd;
-    string word;
-    unordered_map<char, TrieNode*> children;
-    TrieNode() {
-        isEnd = false;
-        word = "*";
-    }
-};
-
-class Trie {
+class Stack {
+    enum { DEF_CAP = 2 };
 public:
-    Trie();
-    TrieNode* root;
-    bool query(string s);
-    void insert(string s);
+    Stack(int cap = DEF_CAP);
+    ~Stack();
+    int size() const;
+    bool empty() const;
+    string top() const;
+    void push(string s);
+    void pop();
+private:
+    string* S;
+    int t;
+    int capacity;
 };
 
-Trie::Trie() { root = new TrieNode(); }
-bool Trie::query(string s) {
-    TrieNode* crawler = root;
-    
-    for (auto& c : s) {
-        if (!crawler->children[c]) return false;
-        crawler = crawler->children[c];
+Stack::Stack(int cap) : S(new string[cap]), capacity(cap), t(-1) { }
+Stack::~Stack() { while (!empty()) pop(); }
+int Stack::size() const { return t + 1; }
+bool Stack::empty() const { return size() == 0; }
+string Stack::top() const { return empty() ? "*" : S[t]; }
+void Stack::push(string s) {
+    if (size() == capacity) {
+        string* T = new string[capacity * 2];
+        for (int i = 0; i < capacity; ++i) T[i] = S[i];
+        S = T;
+        capacity *= 2;
     }
-    return crawler->isEnd;
+    S[++t] = s;
 }
-void Trie::insert(string s) {
-    TrieNode* crawler = root;
-    
-    for (auto& c : s) {
-        if (!crawler->children[c]) {
-            crawler->children[c] = new TrieNode();
-        }
-        crawler = crawler->children[c];
-    }
-    
-    crawler->isEnd = true;
-    crawler->word = s;
-}
+void Stack::pop() { if (!empty()) --t; }
 
 int main() {
-    Trie* t = new Trie();
-    t->insert("Though she");
-    t->insert("be but");
-    t->insert("little,");
-    t->insert("she is");
-    t->insert("fierce!");
+    Stack* st = new Stack();
     
-    cout << t->query("Though she") << "\n";
-    cout << t->query("Though he") << "\n";
-    cout << t->query("be but") << "\n";
-    cout << t->query("little,") << "\n";
-    cout << t->query("she is") << "\n";
-    cout << t->query("fierce!") << "\n";
+    st->push("in ourselves.");
+    st->push("but");
+    st->push("in our stars,");
+    st->push("is not");
+    st->push("dear Brutus,");
+    st->push("The fault,");
+    
+    while (!st->empty()) {
+        cout << st->top() << " ";
+        st->pop();
+    }
     
     std::cout << "\nFIN\n";
     return 0;
