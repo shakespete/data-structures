7f79709333f292bcfc5fb5acc1b87d17cd68fce0
shakespete <petersyjuco@gmail.com>
2022-02-17 09:43:17 +1100

    Doubly Linked List

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -5,49 +5,77 @@
 
 using namespace std;
 
-class Stack {
-    enum { DEF_CAP = 3 };
-public:
-    Stack(int cap = DEF_CAP);
-    ~Stack();
-    int size() const;
-    bool empty() const;
-    string top() const;
-    void push(string s);
-    void pop();
-private:
-    string* S;
-    int t;
-    int capacity;
+struct Node {
+    string val;
+    Node* prev;
+    Node* next;
 };
 
-Stack::Stack(int cap) : S(new string[cap]), t(-1), capacity(cap) { }
-Stack::~Stack() { while (!empty()) pop(); }
-int Stack::size() const { return t + 1; }
-bool Stack::empty() const { return size() == 0; }
-string Stack::top() const { return empty() ? "*" : S[t]; }
-void Stack::push(string s) {
-    if (size() == capacity) {
-        string* T = new string[capacity * 2];
-        for (int i = 0; i < capacity; ++i) T[i] = S[i];
-        S = T;
-        capacity *= 2;
-    }
-    S[++t] = s;
+class DLL {
+public:
+    DLL();
+    ~DLL();
+    bool empty() const;
+    Node* front() const;
+    Node* back() const;
+    void add (Node* v, string s);
+    void addFront(string s);
+    void addBack(string s);
+    void remove(Node* v);
+    void removeFront();
+    void removeBack();
+private:
+    Node* head;
+    Node* tail;
+};
+
+DLL::DLL() {
+    head = new Node();
+    tail = new Node();
+    head->next = tail;
+    tail->prev = head;
+    head->val = "*";
+    tail->val = "*";
 }
-void Stack::pop() { if (!empty()) --t; }
+DLL::~DLL() { while (!empty()) removeFront(); }
+bool DLL::empty() const { return head->next == tail; }
+Node* DLL::front() const { return empty() ? nullptr : head->next; }
+Node* DLL::back() const { return empty() ? nullptr : tail->prev; }
+void DLL::add (Node* v, string s) {
+    Node* u = new Node;
+    u->val = s;
+    
+    u->next = v;
+    u->prev = v->prev;
+    v->prev->next = u;
+    v->prev = u;
+}
+void DLL::addFront(string s) { add(head->next, s); }
+void DLL::addBack(string s) { add(tail, s); }
+void DLL::remove(Node* v) {
+    if (!empty()) {
+        Node* u = v->prev;
+        Node* w = v->next;
+        u->next = w;
+        w->prev = u;
+        delete v;
+    }
+}
+void DLL::removeFront() { if (!empty()) remove(head->next); }
+void DLL::removeBack() { if (!empty()) remove(tail->prev); }
 
 int main() {
-    Stack* st = new Stack();
-    st->push("may be.");
-    st->push("what we");
-    st->push("but not");
-    st->push("what we are,");
-    st->push("We know");
+    DLL* dlist = new DLL();
     
-    while (!st->empty()) {
-        cout << st->top() << " ";
-        st->pop();
+    dlist->addBack("Stars,");
+    dlist->addBack("hide your fires;");
+    dlist->addBack("Let not light");
+    dlist->addBack("see my black");
+    dlist->addBack("and deep desires.");
+    
+    while (!dlist->empty()) {
+        cout << dlist->front()->val << " ";
+        dlist->removeFront();
     }
     
     cout << "\nFIN\n";
