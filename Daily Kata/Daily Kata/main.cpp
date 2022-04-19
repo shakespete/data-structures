b37dbd552750d6646838765be39fcd711a710473
shakespete <petersyjuco@gmail.com>
2022-04-19 23:25:47 +1000

    Doubly Linked List

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,64 +1,81 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
-#include <unordered_map>
 
 using namespace std;
 
 struct Node {
-    unordered_map<char, Node*> children;
-    bool isEnd;
+    string val;
+    Node* prev;
+    Node* next;
 };
 
-class Trie {
+class DLL {
 public:
-    Node* root;
-    Trie();
-    void insert(string s);
-    bool query(string s);
+    DLL();
+    ~DLL();
+    bool empty() const;
+    Node* front() const;
+    Node* back() const;
+    void add(Node* v, string s);
+    void addFront(string s);
+    void addBack(string s);
+    void remove(Node* v);
+    void removeFront();
+    void removeBack();
+private:
+    Node* head;
+    Node* tail;
 };
 
-
-Trie::Trie() { root = new Node(); }
-void Trie::insert(string s) {
-    Node* crawler = root;
-    
-    for (auto letter : s) {
-        if (crawler->children.find(letter) == crawler->children.end()) {
-            Node* node = new Node();
-            crawler->children.insert({ letter, node });
-        }
-        crawler = crawler->children[letter];
-    }
-    crawler->isEnd = true;
+DLL::DLL() {
+    head = new Node();
+    tail = new Node();
+    head->next = tail;
+    tail->prev = head;
+    head->val = "*";
+    tail->val = "*";
 }
-bool Trie::query(string s) {
-    Node* crawler = root;
-    
-    for (auto letter : s) {
-        if (crawler->children.find(letter) == crawler->children.end()) return false;
-        crawler = crawler->children[letter];
-    }
-    return crawler->isEnd;
+DLL::~DLL() { while (!empty()) removeFront(); }
+bool DLL::empty() const { return head->next == tail; }
+Node* DLL::front() const { return empty() ? nullptr : head->next; }
+Node* DLL::back() const { return empty() ? nullptr : tail->prev; }
+void DLL::add(Node* v, string s) {
+    Node* u = new Node;
+    u->val = s;
+    u->next = v;
+    u->prev = v->prev;
+    v->prev->next = u;
+    v->prev = u;
 }
+void DLL::addFront(string s) { add(head->next, s); }
+void DLL::addBack(string s) { add(tail, s); }
+void DLL::remove(Node* v) {
+    if (!empty()) {
+        Node* u = v->prev;
+        Node* w = v->next;
+        u->next = w;
+        w->prev = u;
+        delete v;
+    }
+}
+void DLL::removeFront() { if (!empty()) remove(head->next); }
+void DLL::removeBack() { if (!empty()) remove(tail->prev); }
 
 int main() {
-    Trie* t = new Trie();
+    DLL* dlist = new DLL();
+    dlist->addBack("O, beware,");
+    dlist->addBack("my lord,");
+    dlist->addBack("of jealousy;");
+    dlist->addBack("it is the green-ey’d monster,");
+    dlist->addBack("which doth mock");
+    dlist->addBack("the meat it feeds on.");
     
-    t->insert("The devil");
-    t->insert("can cite");
-    t->insert("Scripture");
-    t->insert("for his");
-    t->insert("purpose");
+    while (!dlist->empty()) {
+        cout << dlist->front()->val << " ";
+        dlist->removeFront();
+    }
     
-    cout << t->query("The devil") << "\n";
-    cout << t->query("can cite") << "\n";
-    cout << t->query("Scripture") << "\n";
-    cout << t->query("for his") << "\n";
-    cout << t->query("purpose") << "\n";
-    
-    cout << t->query("scripture") << "\n";
-    
-    std::cout << "FIN\n";
+    std::cout << "\nFIN\n";
     return 0;
 }
