2c4ace01e62fa8ac04ffdb575942e520abd62d72
shakespete <petersyjuco@gmail.com>
2022-05-05 17:26:35 +1000

    Trie

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,66 +1,60 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
-struct Node {
-    string val;
+struct TrieNode {
+    unordered_map<char, TrieNode*> children;
+    bool isEnd;
 };
 
-class Stack {
-    enum { DEF_CAP = 2 };
+class Trie {
 public:
-    Stack(int cap = DEF_CAP);
-    ~Stack();
-    int size() const;
-    bool empty() const;
-    Node* top() const;
-    void push(string s);
-    void pop();
-private:
-    Node** S;
-    int t;
-    int capacity;
+    Trie();
+    TrieNode* root;
+    void insert(string s);
+    bool query(string s);
 };
 
-Stack::Stack(int cap) : S(new Node*[cap]), t(-1), capacity(cap) { }
-Stack::~Stack() { while (!empty()) pop(); }
-int Stack::size() const { return t + 1; }
-bool Stack::empty() const { return t == -1; }
-Node* Stack::top() const { return empty() ? nullptr : S[t]; }
-void Stack::push(string s) {
-    if (size() == capacity) {
-        Node** T = new Node*[capacity * 2];
-        for (int i = 0; i < capacity; ++i) T[i] = S[i];
-        S = T;
-        capacity *= 2;
+Trie::Trie() { root = new TrieNode(); }
+void Trie::insert(string s) {
+    TrieNode* crawler = root;
+    
+    for (auto letter : s) {
+        if (crawler->children.find(letter) == crawler->children.end()) {
+            TrieNode* node = new TrieNode();
+            crawler->children.insert({ letter, node });
+        }
+        crawler = crawler->children[letter];
     }
-    Node* node = new Node();
-    node->val = s;
-    S[++t] = node;
+    crawler->isEnd = true;
 }
-void Stack::pop() {
-    if (!empty()) {
-        delete S[t];
-        --t;
+bool Trie::query(string s) {
+    TrieNode* crawler = root;
+    
+    for (auto letter : s) {
+        if (crawler->children.find(letter) == crawler->children.end()) return false;
+        crawler = crawler->children[letter];
     }
+    return crawler->isEnd;
 }
+
 
 int main() {
-    Stack* st = new Stack();
-    st->push("wit");
-    st->push("foolish");
-    st->push("than a");
-    st->push("fool");
-    st->push("a witty");
-    st->push("Better");
+    Trie* t = new Trie();
+    t->insert("We know");
+    t->insert("what we are,");
+    t->insert("but know not");
+    t->insert("what we may be.");
     
-    while (!st->empty()) {
-        cout << st->top()->val << " ";
-        st->pop();
-    }
+    cout << t->query("We know") << "\n";
+    cout << t->query("what we are,") << "\n";
+    cout << t->query("but know not") << "\n";
+    cout << t->query("what we may be") << "\n";
+    cout << t->query("what we may be.") << "\n";
     
     std::cout << "\nFIN\n";
     return 0;
