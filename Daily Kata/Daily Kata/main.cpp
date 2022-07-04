7a6e67e5b4b39cc17a6a585e9c76a60013ccfe32
shakespete <petersyjuco@gmail.com>
2022-07-04 21:12:04 +1000

    BST

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,60 +1,139 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <string>
+#include <stack>
 #include <vector>
 
 using namespace std;
 
-struct Node {
-    string val;
-    Node* next;
-};
-
-class LinkedList {
+class BST {
 public:
-    LinkedList();
-    ~LinkedList();
-    bool empty() const;
-    Node* front() const;
-    void add(string s);
-    void remove();
-private:
-    Node* head;
+    int val;
+    BST* left;
+    BST* right;
+    BST(int e);
+    BST* treeMin(BST* x);
+    BST* treeSearch(int e);
+    BST* insert(int e);
+    BST* remove(int e, BST* p);
+    void inorderTraversal();
 };
 
-LinkedList::LinkedList() : head(nullptr) { }
-LinkedList::~LinkedList() { while (!empty()) remove(); }
-bool LinkedList::empty() const { return head == nullptr; }
-Node* LinkedList::front() const { return empty() ? nullptr : head; }
-void LinkedList::add(string s) {
-    Node* v = new Node();
-    v->val = s;
-    v->next = head;
-    head = v;
+BST::BST(int e) {
+    val = e;
+    left = nullptr;
+    right = nullptr;
 }
-void LinkedList::remove() {
-    if (!empty()) {
-        Node* old = head;
-        head = old->next;
-        delete old;
+BST* BST::treeMin(BST* x) {
+    while (x->left) x = x->left;
+    return x;
+}
+BST* BST::treeSearch(int e) {
+    BST* x = this;
+    while (x && x->val != e) {
+        if (e < x->val) x = x->left;
+        else x = x->right;
+    }
+    return x;
+}
+BST* BST::insert(int e) {
+    BST* x = this;
+    while (true) {
+        if (e < x->val) {
+            if (x->left) x = x->left;
+            else {
+                BST* node = new BST(e);
+                x->left = node;
+                break;
+            }
+        } else {
+            if (x->right) x = x->right;
+            else {
+                BST* node = new BST(e);
+                x->right = node;
+                break;
+            }
+        }
+    }
+    return x;
+}
+BST* BST::remove(int e, BST* p = nullptr) {
+    BST* x = this;
+    while (x) {
+        if (e < x->val) {
+            p = x;
+            x = x->left;
+        } else if (e > x->val) {
+            p = x;
+            x = x->right;
+        } else {
+            if (x->left && x->right) {
+                BST* min = treeMin(x->right);
+                x->val = min->val;
+                x->right->remove(min->val, x);
+            } else if (!p) {
+                if (x->left) {
+                    x->val = x->left->val;
+                    x->right = x->left->right;
+                    x->left = x->left->left;
+                } else if (x->right) {
+                    x->val = x->right->val;
+                    x->left = x->right->left;
+                    x->right = x->right->right;
+                } else {
+                    x = nullptr;
+                }
+            } else if (p->left == x) {
+                p->left = x->left ? x->left : x->right;
+            } else if (p->right == x) {
+                p->right = x->left ? x->left : x->right;
+            }
+            break;
+        }
+    }
+    return x;
+}
+void BST::inorderTraversal() {
+    stack<BST*> st;
+    BST* x = this;
+    
+    while (x || !st.empty()) {
+        while (x) {
+            st.push(x);
+            x = x->left;
+        }
+        
+        x = st.top();
+        st.pop();
+        
+        cout << x->val << " ";
+        x = x->right;
     }
 }
+
 
 int main() {
-    LinkedList* list = new LinkedList();
+    BST *root = new BST(10);
+    root->left = new BST(5);
+    root->left->left = new BST(2);
+    root->left->left->left = new BST(1);
+    root->left->right = new BST(5);
+    root->right = new BST(15);
+    root->right->left = new BST(13);
+    root->right->left->right = new BST(14);
+    root->right->right = new BST(22);
     
-    list->add("don't see");
-    list->add("The sentimental fool ");
-    list->add("long ago\n");
-    list->add("in her ");
-    list->add("somewhere back ");
-    list->add("He came from ");
+    cout << "Inorder: ";
+    root->inorderTraversal();
+    cout << "\n";
+    root->insert(12);
+    root->inorderTraversal();
+    cout << "\n";
     
-    while (!list->empty()) {
-        cout << list->front()->val;
-        list->remove();
-    }
-    
+    root->remove(1);
+    root->remove(14);
+    root->remove(12);
+    root->inorderTraversal();
+    cout << "\n";
     std::cout << "\nFIN\n";
     return 0;
 }
