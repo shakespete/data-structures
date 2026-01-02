10bfbcee6862241c434e6ea9fc087c949a9e506e
Pete <shakespete@gmail.com>
2026-01-02 11:48:49 +1100

    Dynamic array cleanup

--- Dynamic Array/dynamic_array.cpp
+++ Dynamic Array/dynamic_array.cpp
@@ -1,14 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdexcept>
 
 class Stack {
     enum { DEF_CAP = 100 };
 public:
-    Stack(int e = DEF_CAP);
+    Stack(int cap = DEF_CAP);
     ~Stack();
-    int size();
-    bool empty();
-    bool find(int e);
+    int size() const;
+    bool empty() const;
+    bool find(int e) const;
+    int top() const;
     void push(int e);
     int pop();
 private:
@@ -16,16 +18,19 @@ private:
     int capacity;
     int n;
 };
-Stack::Stack(int e) : capacity(e), A(new int[e]), n(0) { }
+Stack::Stack(int cap): capacity(cap), A(new int[cap]), n(0) { }
 Stack::~Stack() { delete[] A; }
-int Stack::size() { return n; }
-bool Stack::empty() { return n == 0; }
-bool Stack::find(int e) {
-    if (empty()) return false;
-    for (int i = 0; i < n; ++i)
+int Stack::size() const { return n; }
+bool Stack::empty() const { return n == 0; }
+bool Stack::find(int e) const {
+    for (int i = 0; i < n; ++i) {
         if (A[i] == e) return true;
-
+    }
     return false;
+}
+int Stack::top() const {
+    if (empty()) throw std::runtime_error("Stack is empty");
+    return A[n - 1];
 }
 void Stack::push(int e) {
     if (size() == capacity) {
@@ -38,28 +43,30 @@ void Stack::push(int e) {
     A[n++] = e;
 }
 int Stack::pop() {
-    if (empty()) return -1;
+    if (empty()) throw std::runtime_error("Stack is empty");
     return A[--n];
 }
 
 int main() {
-    Stack* st = new Stack(5);
-    st->push(0);
-    st->push(1);
-    st->push(2);
-    st->push(3);
-    st->push(4);
-    st->push(5);
-    st->push(6);
-    st->push(7);
-    st->push(8);
-    st->push(9);
-    
-    printf("Size: %d\n", st->size());
-    printf("Find 9: %d\n", st->find(9));
-    printf("Top: %d\n", st->pop());
-    printf("Find 9: %d\n", st->find(9));
-    printf("Find 8: %d\n", st->find(8));
+    Stack st(5);
+    st.push(0);
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+    st.push(6);
+    st.push(7);
+    st.push(8);
+    st.push(9);
 
+    printf("Size: %d\n", st.size());
+    printf("Find 9: %d\n", st.find(9));
+    printf("Top: %d\n", st.top());
+    printf("Pop: %d\n", st.pop());
+    printf("Find 9: %d\n", st.find(9));
+    printf("Top: %d\n", st.top());
+
+    printf("\nFIN\n");
     return 0;
 }
